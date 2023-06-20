08f155a5519b57f7d6adc5927b8ce7c586a63304
Taranjeet Singh <reachtotj@gmail.com>
2023-06-20 16:50:54 +0530

    Update website to web page
    This commit renames the website loader, chunker
    to web page, as it is loading and chunking a single
    url than the complete website.

--- embedchain/chunkers/web_page.py
+++ embedchain/chunkers/web_page.py
@@ -10,7 +10,7 @@ TEXT_SPLITTER_CHUNK_PARAMS = {
 }
 
 
-class WebsiteChunker(BaseChunker):
+class WebPageChunker(BaseChunker):
     def __init__(self):
         text_splitter = RecursiveCharacterTextSplitter(**TEXT_SPLITTER_CHUNK_PARAMS)
         super().__init__(text_splitter)
