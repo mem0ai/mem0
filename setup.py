963cc671fe1c115ee8e047c597810db499b589ca
Taranjeet Singh <reachtotj@gmail.com>
2023-06-20 17:41:57 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="embedchain",
+    version="0.0.2",
+    author="Taranjeet Singh",
+    author_email="reachtotj@gmail.com",
+    description="embedchain is a framework to easily create bots over any dataset",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/embedchain/embedchain",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    py_modules=["embedchain"],
+    install_requires=[
+        "langchain>=0.0.205",
+        "requests",
+        "openai",
+        "chromadb",
+        "youtube-transcript-api",
+        "beautifulsoup4",
+        "pypdf",
+        "pytube",
+    ]
+)
