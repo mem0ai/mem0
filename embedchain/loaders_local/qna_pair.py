e60f1680a0f06f582bda9a9b08004fcb47afc468
cachho <admin@ch-webdev.com>
2023-06-23 14:24:43 +0200

    chore: removed markdown to plaintext function

--- embedchain/loaders_local/qna_pair.py
+++ embedchain/loaders_local/qna_pair.py
@@ -1,11 +1,7 @@
-from embedchain.utils import markdown_to_plaintext
-
-
 class QnaPairLoader:
 
     def load_data(self, content):
         question, answer = content
-        answer = markdown_to_plaintext(answer)
         content = f"Q: {question}\nA: {answer}"
         meta_data = {
             "url": "local",
