7d52406c30286c407c64938da7439dcbe012e5e3
cachho <admin@ch-webdev.com>
2023-06-23 16:44:20 +0200

    docs: added function documentation

--- embedchain/utils.py
+++ embedchain/utils.py
@@ -2,9 +2,38 @@ import re
 
 
 def clean_string(text):
+    """
+    This function takes in a string and performs a series of text cleaning operations. 
+
+    Args:
+        text (str): The text to be cleaned. This is expected to be a string.
+
+    Returns:
+        cleaned_text (str): The cleaned text after all the cleaning operations have been performed.
+    """
+    # Replacement of newline characters:
+    # This replaces all newline characters '\n' in the text with a space character ' '.
     text = text.replace('\n', ' ')
+    
+    # Stripping and reducing multiple spaces to single:
+    # The `strip()` function removes any leading or trailing spaces in the string. 
+    # The `re.sub(r'\s+', ' ', text)` uses a regular expression (regex) to find and replace 
+    # all occurrences of one or more whitespace characters (\s+) in the string with a single space ' '.
     cleaned_text = re.sub(r'\s+', ' ', text.strip())
+    
+    # Removing backslashes:
+    # This replaces all backslashes '\\' in the string with nothing, effectively removing them.
     cleaned_text = cleaned_text.replace('\\', '')
+    
+    # Replacing hash characters:
+    # This replaces all hash/pound characters '#' in the string with a space character ' '.
     cleaned_text = cleaned_text.replace('#', ' ')
+    
+    # Eliminating consecutive non-alphanumeric characters:
+    # This regex identifies consecutive non-alphanumeric characters (i.e., not a word character [a-zA-Z0-9_] and not a whitespace) in the string 
+    # and replaces each group of such characters with a single occurrence of that character. 
+    # For example, "!!! hello !!!" would become "! hello !".
     cleaned_text = re.sub(r'([^\w\s])\1*', r'\1', cleaned_text)
+    
     return cleaned_text
+
