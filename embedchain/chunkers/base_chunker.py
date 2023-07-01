3ab994c0ff60a665d14fb7cc1efba4052dd4c8a9
cachho <admin@ch-webdev.com>
2023-07-01 12:26:44 +0200

    refactor: use src instead of url

--- embedchain/chunkers/base_chunker.py
+++ embedchain/chunkers/base_chunker.py
@@ -5,16 +5,18 @@ class BaseChunker:
     def __init__(self, text_splitter):
         self.text_splitter = text_splitter
 
-    def create_chunks(self, loader, url):
+    def create_chunks(self, loader, src):
         documents = []
         ids = []
-        datas = loader.load_data(url)
+        datas = loader.load_data(src)
         metadatas = []
         for data in datas:
             content = data["content"]
             meta_data = data["meta_data"]
-            chunks = self.text_splitter.split_text(content)
             url = meta_data["url"]
+
+            chunks = self.text_splitter.split_text(content)
+
             for chunk in chunks:
                 chunk_id = hashlib.sha256((chunk + url).encode()).hexdigest()
                 ids.append(chunk_id)
