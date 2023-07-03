1f1cfe0ff77ab4efe078d445b17bc2650df030a8
missioncfwf <missioncfwf@yahoo.com>
2023-07-03 09:56:15 +0100

    Update youtube_video.py
    error if "No data found"
    
    wrap error in try function and print "Error loading data from YouTube video", then empty list returned if error (for the video and not the playlist)

--- embedchain/loaders/youtube_video.py
+++ embedchain/loaders/youtube_video.py
@@ -1,22 +1,24 @@
 from langchain.document_loaders import YoutubeLoader
-
 from embedchain.utils import clean_string
 
-
 class YoutubeVideoLoader:
-
     def load_data(self, url):
-        loader = YoutubeLoader.from_youtube_url(url, add_video_info=True)
-        doc = loader.load()
-        output = []
-        if not len(doc):
-            raise ValueError("No data found")
-        content = doc[0].page_content
-        content = clean_string(content)
-        meta_data = doc[0].metadata
-        meta_data["url"] = url
-        output.append({
-            "content": content,
-            "meta_data": meta_data,
-        })
-        return output
+        try:
+            loader = YoutubeLoader.from_youtube_url(url, add_video_info=True)
+            doc = loader.load()
+            output = []
+            if not len(doc):
+                raise ValueError("No data found")
+            content = doc[0].page_content
+            content = clean_string(content)
+            meta_data = doc[0].metadata
+            meta_data["url"] = url
+            output.append({
+                "content": content,
+                "meta_data": meta_data,
+            })
+            return output
+        except Exception as e:
+            print(f"Error loading data from YouTube video: {str(e)}")
+            return []  # Return an empty list if there is an error
+
