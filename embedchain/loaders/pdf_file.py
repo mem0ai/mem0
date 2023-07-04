cc5c96689b7aaaf42e7f57ae2b15cb15e1922101
Sahil Kumar Yadav <sahilyadav902@gmail.com>
2023-07-04 23:48:23 +0530

    Update pdf_file.py

--- embedchain/loaders/pdf_file.py
+++ embedchain/loaders/pdf_file.py
@@ -15,10 +15,8 @@ class PdfFileLoader:
             content = clean_string(content)
             meta_data = page.metadata
             meta_data["url"] = url
-            output.append(
-                {
-                    "content": content,
-                    "meta_data": meta_data,
-                }
-            )
+            output.append({
+                "content": content,
+                "meta_data": meta_data,
+            })
         return output
