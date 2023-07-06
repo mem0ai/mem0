ee38b26b1846ba32a6f10667c4831d2c73be016f
cachho <admin@ch-webdev.com>
2023-07-06 20:45:17 +0200

    docs: add number_documents

--- embedchain/config/QueryConfig.py
+++ embedchain/config/QueryConfig.py
@@ -5,6 +5,9 @@ class QueryConfig(BaseConfig):
     Config for the `query` method.
     """
     def __init__(self, number_documents=None):
+        """
+        :param number_documents: Number of documents to pull from the database as context.
+        """
         if number_documents is None:
             self.number_documents = 1
         else:
