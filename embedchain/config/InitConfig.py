f0c9d1e51e7acdc99f8b49d9415d9c5d9a63365a
cachho <admin@ch-webdev.com>
2023-07-06 14:28:28 +0200

    feat: added default

--- embedchain/config/InitConfig.py
+++ embedchain/config/InitConfig.py
@@ -1,5 +1,30 @@
-import BaseConfig
+import os
+
+from embedchain.config.BaseConfig import BaseConfig
+
+from embedchain.vectordb.chroma_db import ChromaDB
+from chromadb.utils import embedding_functions
 
 class InitConfig(BaseConfig):
-    def __init__(self):
-        pass
+    def __init__(self, ef=None, db=None):
+        # Embedding Function
+        if ef is None:
+            self.ef = embedding_functions.OpenAIEmbeddingFunction(
+                api_key=os.getenv("OPENAI_API_KEY"),
+                organization_id=os.getenv("OPENAI_ORGANIZATION"),
+                model_name="text-embedding-ada-002"
+            )
+        else:
+            self.ef = ef
+
+        if db is None:
+            self.db = ChromaDB(ef=self.ef)
+        else:
+            self.db = db
+
+        return
+
+
+    def _set_embedding_function(self, ef):
+        self.ef = ef
+        return        
