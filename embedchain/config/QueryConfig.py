b923c49d8c265dcc0f919e65ef8245b21076e370
ahnedeee <136900735+ahnedeee@users.noreply.github.com>
2023-07-10 05:09:54 -0400

    Update QueryConfig.py

--- embedchain/config/QueryConfig.py
+++ embedchain/config/QueryConfig.py
@@ -29,6 +29,7 @@ class QueryConfig(BaseConfig):
         :param temperature: Optional. Controls the randomness of the model's output. 
                             Higher values (closer to 1) make output more random, lower values make it more deterministic.
         :param max_tokens: Optional. Controls how many tokens are generated.
+        :param model: Optional. Controls the OpenAI model used.
         :raises ValueError: If the template is not valid as template should contain $context and $query
         """
         if template is None:
