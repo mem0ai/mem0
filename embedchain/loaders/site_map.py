7cb75549d39f7b75cc961630ab4d1cfa4c8eab91
Aaishik Dutta <aaishikdutta@Aaishiks-MacBook-Pro.local>
2023-07-11 20:48:58 +0530

    added sitemap modified

--- embedchain/loaders/site_map.py
+++ embedchain/loaders/site_map.py
@@ -1,14 +1,15 @@
 import requests
-
 from bs4 import BeautifulSoup
+
 from embedchain.loaders.web_page import WebPageLoader
+
 
 class SitemapLoader:
     def load_data(self, sitemap_url):
         """
-            This method takes a sitemap url as input and retrieves
-            all the urls to use the WebPageLoader to load content
-            of each page.
+        This method takes a sitemap url as input and retrieves
+        all the urls to use the WebPageLoader to load content
+        of each page.
         """
         output = []
         web_page_loader = WebPageLoader()
@@ -17,16 +18,16 @@ class SitemapLoader:
 
         if response.status_code == 200:
             soup = BeautifulSoup(response.text, features="xml")
-            links = [link.text for link in soup.find_all('loc')]
+            links = [link.text for link in soup.find_all("loc")]
 
             for link in links:
                 each_load_data = web_page_loader.load_data(link)
-                # WebPageLoader returns a list with single element which is extracted and appended to 
-                # the output list containing data for all pages
+                # WebPageLoader returns a list with single element
+                # which is extracted and appended to the output list
+                # containing data for all pages
                 output.append(each_load_data[0])
 
             return output
-        
+
         else:
             raise response.raise_for_status()
-
