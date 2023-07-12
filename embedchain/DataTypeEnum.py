b9e966b32b95ef165fb753d220ae9b981cf48ff4
zjzj1996 <guaguaguaxia@gmail.com>
2023-07-12 16:56:11 +0800

    feat:add enum

---
+++ embedchain/DataTypeEnum.py
@@ -0,0 +1,18 @@
+from enum import Enum, unique
+
+@unique
+class DataType(Enum):
+    YOUTUBE_VIDEO = "youtube_video"
+    PDF_FILE = "pdf_file"
+    WEB_PAGE = "web_page"
+    QNA_PAIR = "qna_pair"
+    TEXT = "text"
+    DOC = "docx"
+
+
+    @staticmethod
+    def get_enum(enum_str):
+        for member in DataType:
+            if member.value == enum_str:
+                return member
+        raise ValueError(f"Invalid enum string: {enum_str}")
