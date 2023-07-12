49d10b0dcd989b54a2c2ae1e7f78aeaf55440b94
Aaishik Dutta <aaishikdutta@Aaishiks-MacBook-Pro.local>
2023-07-12 10:46:44 +0530

    added refactor and lint format fixes

--- embedchain/loaders/site_map.py
+++ embedchain/loaders/site_map.py
@@ -7,27 +7,18 @@ from embedchain.loaders.web_page import WebPageLoader
 class SitemapLoader:
     def load_data(self, sitemap_url):
         """
-        This method takes a sitemap url as input and retrieves
-        all the urls to use the WebPageLoader to load content
+        This method takes a sitemap URL as input and retrieves
+        all the URLs to use the WebPageLoader to load content
         of each page.
         """
         output = []
         web_page_loader = WebPageLoader()
-
         response = requests.get(sitemap_url)
+        response.raise_for_status()
 
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, features="xml")
-            links = [link.text for link in soup.find_all("loc")]
-
-            for link in links:
-                each_load_data = web_page_loader.load_data(link)
-                # WebPageLoader returns a list with single element
-                # which is extracted and appended to the output list
-                # containing data for all pages
-                output.append(each_load_data[0])
-
-            return output
-
-        else:
-            raise response.raise_for_status()
+        soup = BeautifulSoup(response.text, "xml")
+        links = [link.text for link in soup.find_all("loc")]
+        for link in links:
+            each_load_data = web_page_loader.load_data(link)
+            output.append(each_load_data)
+        return [data[0] for data in output]
