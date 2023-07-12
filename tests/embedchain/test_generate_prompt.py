2fabe88bc412390cad942489b0f16c35bb9e8311
cachho <admin@ch-webdev.com>
2023-07-12 18:51:48 +0200

    test: added test for history

--- tests/embedchain/test_generate_prompt.py
+++ tests/embedchain/test_generate_prompt.py
@@ -5,6 +5,7 @@ from string import Template
 
 from embedchain import App
 from embedchain.embedchain import QueryConfig
+from unittest.mock import patch
 
 
 class TestGeneratePrompt(unittest.TestCase):
@@ -57,3 +58,15 @@ class TestGeneratePrompt(unittest.TestCase):
             context="Context 1 | Context 2 | Context 3", query=input_query
         )
         self.assertEqual(result, expected_result)
+
+
+    def test_generate_prompt_with_history(self):
+        """
+        Test the 'generate_prompt' method with QueryConfig containing a history attribute.
+        """
+        config = QueryConfig(history=["Past context 1", "Past context 2"])
+        config.template = Template("Context: $context | Query: $query | History: $history")
+        prompt = self.app.generate_prompt("Test query", ["Test context"], config)
+
+        expected_prompt = "Context: Test context | Query: Test query | History: ['Past context 1', 'Past context 2']"
+        self.assertEqual(prompt, expected_prompt)
