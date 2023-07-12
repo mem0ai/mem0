54bfcba0b15ddd177c779216e6c5f41cb3b6e737
cachho <admin@ch-webdev.com>
2023-07-12 20:40:29 +0200

    test: add tests

--- tests/vectordb/test_chroma_db.py
+++ tests/vectordb/test_chroma_db.py
@@ -1,11 +1,14 @@
+# ruff: noqa: E501
+
 import unittest
 from unittest.mock import patch
 
-from embedchain.vectordb.chroma_db import ChromaDB
-from embedchain.vectordb.chroma_db import chromadb
+from embedchain import App
+from embedchain.config import InitConfig
+from embedchain.vectordb.chroma_db import ChromaDB, chromadb
 
 
-class TestChromaDB(unittest.TestCase):
+class TestChromaDbHosts(unittest.TestCase):
     def test_init_with_host_and_port(self):
         """
         Test if the `ChromaDB` instance is initialized with the correct host and port values.
@@ -14,7 +17,7 @@ class TestChromaDB(unittest.TestCase):
         port = "1234"
 
         with patch.object(chromadb, "Client") as mock_client:
-            db = ChromaDB(host=host, port=port)
+            _db = ChromaDB(host=host, port=port)
 
         expected_settings = chromadb.config.Settings(
             chroma_api_impl="rest",
@@ -25,5 +28,45 @@ class TestChromaDB(unittest.TestCase):
         mock_client.assert_called_once_with(expected_settings)
 
 
-if __name__ == "__main__":
-    unittest.main()
+class TestChromaDbHostsInit(unittest.TestCase):
+    @patch("embedchain.vectordb.chroma_db.chromadb.Client")
+    def test_init_with_host_and_port(self, mock_client):
+        """
+        Test if the `App` instance is initialized with the correct host and port values.
+        """
+        host = "test-host"
+        port = "1234"
+
+        config = InitConfig(host=host, port=port)
+
+        _app = App(config)
+
+        self.assertEqual(mock_client.call_args[0][0].chroma_server_host, host)
+        self.assertEqual(mock_client.call_args[0][0].chroma_server_http_port, port)
+
+
+class TestChromaDbHostsNone(unittest.TestCase):
+    @patch("embedchain.vectordb.chroma_db.chromadb.Client")
+    def test_init_with_host_and_port(self, mock_client):
+        """
+        Test if the `App` instance is initialized without default hosts and ports.
+        """
+
+        _app = App()
+
+        self.assertEqual(mock_client.call_args[0][0].chroma_server_host, None)
+        self.assertEqual(mock_client.call_args[0][0].chroma_server_http_port, None)
+
+
+class TestChromaDbHostsLoglevel(unittest.TestCase):
+    @patch("embedchain.vectordb.chroma_db.chromadb.Client")
+    def test_init_with_host_and_port(self, mock_client):
+        """
+        Test if the `App` instance is initialized without a config that does not contain default hosts and ports.
+        """
+        config = InitConfig(log_level="DEBUG")
+
+        _app = App(config)
+
+        self.assertEqual(mock_client.call_args[0][0].chroma_server_host, None)
+        self.assertEqual(mock_client.call_args[0][0].chroma_server_http_port, None)
