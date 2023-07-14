26279dceaf0721717311e534787a6cdcd9f5a74a
cachho <admin@ch-webdev.com>
2023-07-14 22:06:06 +0200

    chore: load from config class

--- embedchain/chunkers/pdf_file.py
+++ embedchain/chunkers/pdf_file.py
@@ -5,18 +5,14 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from embedchain.chunkers.base_chunker import BaseChunker
 from embedchain.config.AddConfig import ChunkerConfig
 
-TEXT_SPLITTER_CHUNK_PARAMS = {
-    "chunk_size": 1000,
-    "chunk_overlap": 0,
-    "length_function": len,
-}
-
 
 class PdfFileChunker(BaseChunker):
     """Chunker for PDF file."""
 
     def __init__(self, config: Optional[ChunkerConfig] = None):
         if config is None:
-            config = TEXT_SPLITTER_CHUNK_PARAMS
-        text_splitter = RecursiveCharacterTextSplitter(**config)
+            config = ChunkerConfig(
+                chunk_size=1000, chunk_overlap=0, length_function=len
+            )
+        text_splitter = RecursiveCharacterTextSplitter(config.as_dict())
         super().__init__(text_splitter)
