91ac4e711fecc32d305fa2d00275d5a72bc5a077
cachho <admin@ch-webdev.com>
2023-07-14 22:05:49 +0200

    chore: add dict export and move defaults

--- embedchain/config/AddConfig.py
+++ embedchain/config/AddConfig.py
@@ -10,13 +10,20 @@ class ChunkerConfig(BaseConfig):
 
     def __init__(
         self,
-        chunk_size: Optional[int] = 4000,
-        chunk_overlap: Optional[int] = 200,
-        length_function: Optional[Callable[[str], int]] = len,
+        chunk_size: Optional[int] = None,
+        chunk_overlap: Optional[int] = None,
+        length_function: Optional[Callable[[str], int]] = None,
     ):
-        self.chunk_size = chunk_size
-        self.chunk_overlap = chunk_overlap
-        self.length_function = length_function
+        self.chunk_size = 2000 if chunk_size is None else chunk_size
+        self.chunk_overlap = 0 if chunk_overlap is None else chunk_overlap
+        self.length_function = len if length_function is None else length_function
+
+    def as_dict(self):
+        return {
+            "chunk_size": self.chunk_size,
+            "chunk_overlap": self.chunk_overlap,
+            "length_function": self.length_function,
+        }
 
 
 class LoaderConfig(BaseConfig):
