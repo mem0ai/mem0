69957e08ccac93366ed6f6adf9d8eed956a2e7f5
Taranjeet Singh <reachtotj@gmail.com>
2023-07-15 09:15:18 +0530

    Bump version to 0.0.23 (#277)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="embedchain",
-    version="0.0.22",
+    version="0.0.23",
     author="Taranjeet Singh",
     author_email="reachtotj@gmail.com",
     description="embedchain is a framework to easily create LLM powered bots over any dataset",  # noqa:E501
