45b51a7300ae54e2ad2a5ccc8fe66bd5438d1861
cachho <admin@ch-webdev.com>
2023-07-18 11:43:19 +0200

    feat: anthrophic support

--- embedchain/apps/CustomApp.py
+++ embedchain/apps/CustomApp.py
@@ -4,6 +4,13 @@ from embedchain.config import ChatConfig, CustomAppConfig
 from embedchain.embedchain import EmbedChain
 from embedchain.models import LlmModels
 
+from langchain.schema import BaseMessage
+
+from typing import List
+
+import sys
+import traceback
+
 
 class CustomApp(EmbedChain):
     """
@@ -31,34 +38,61 @@ class CustomApp(EmbedChain):
         self.llm_model = llm_model
 
     def get_llm_model_answer(self, prompt, config: ChatConfig):
-        if self.llm_model == LlmModels.OPENAI:
-            from langchain.callbacks import AsyncIteratorCallbackHandler
-            from langchain.chat_models import ChatOpenAI
-            from langchain.schema import HumanMessage, SystemMessage
-
-            callback_handler = AsyncIteratorCallbackHandler() if config.stream else None
-
-            chat = ChatOpenAI(
-                temperature=config.temperature,
-                model=config.model,
-                max_tokens=config.max_tokens,
-                streaming=config.stream,
-                callbacks=[callback_handler],
+        # TODO: Quitting the streaming response here for now.
+        # Idea: https://gist.github.com/jvelezmagic/03ddf4c452d011aae36b2a0f73d72f68
+        if config.stream:
+            raise NotImplementedError(
+                "Streaming responses have not been implemented for this model yet. Please disable."
             )
-            messages = [
-                SystemMessage(content="You are a helpful assistant."),
-                HumanMessage(content=prompt),
-            ]
 
-            # TODO: Quitting the streaming response here for now.
-            # Idea: https://gist.github.com/jvelezmagic/03ddf4c452d011aae36b2a0f73d72f68
-            if config.stream:
-                raise NotImplementedError(
-                    "Streaming responses have not been implemented for this model yet. Please disable."
+        try:
+            if self.llm_model == LlmModels.OPENAI:
+                from langchain.callbacks import AsyncIteratorCallbackHandler
+                from langchain.chat_models import ChatOpenAI
+
+                callback_handler = AsyncIteratorCallbackHandler() if config.stream else None
+
+                chat = ChatOpenAI(
+                    temperature=config.temperature,
+                    model=config.model,
+                    max_tokens=config.max_tokens,
+                    streaming=config.stream,
+                    callbacks=[callback_handler],
                 )
 
-            response = chat(messages)
-            return response.content
+                if config.top_p and config.top_p != 1:
+                    logging.warning("Config option `top_p` is not supported by this model.")
+
+                messages = CustomApp._get_messages(prompt)
+
+                return chat(messages).content
+            
+            if self.llm_model == LlmModels.ANTHROPHIC:
+                from langchain.chat_models import ChatAnthropic
+
+                chat = ChatAnthropic(temperature=config.temperature,
+                    model=config.model,
+                    max_tokens=config.max_tokens)
+                
+                if config.max_tokens and config.max_tokens != 1000:
+                    logging.warning("Config option `max_tokens` is not supported by this model.")
+
+                messages = CustomApp._get_messages(prompt)
+
+                return chat(messages).content
+        except ImportError as e:
+            raise ImportError(e.msg) from None
+        
+    @staticmethod
+    def _get_messages(prompt: str) -> List[BaseMessage]:
+        from langchain.schema import HumanMessage, SystemMessage
+        return [
+            SystemMessage(content="You are a helpful assistant."),
+            HumanMessage(
+                content=prompt
+            )
+        ]
+
 
     def _stream_llm_model_response(self, response):
         """
