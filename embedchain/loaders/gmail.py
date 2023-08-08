8a92888b81f25cbcb2d92c95b9f68ec6f9ed1b77
cachho <admin@ch-webdev.com>
2023-08-08 23:40:43 +0200

    feat: polished loader

--- embedchain/loaders/gmail.py
+++ embedchain/loaders/gmail.py
@@ -1,38 +1,60 @@
 import logging
+import quopri
+from datetime import datetime
 
 import requests
 from bs4 import BeautifulSoup
 
+try:
+    from llama_index import download_loader
+except ImportError:
+    raise ImportError("Notion requires extra dependencies. Install with `pip install embedchain[community]`") from None
+
 from embedchain.loaders.base_loader import BaseLoader
 from embedchain.utils import clean_string
+
+
+def get_header(text: str, header: str) -> str:
+    start_string_position = text.find(header)
+    pos_start = text.find(":", start_string_position) + 1
+    pos_end = text.find("\n", pos_start)
+    header = text[pos_start:pos_end]
+    return header.strip()
 
 
 class GmailLoader(BaseLoader):
     def load_data(self, query):
         """Load data from gmail."""
 
-        from llama_index import download_loader
-
         GmailReader = download_loader("GmailReader")
         loader = GmailReader(query=query)
         documents = loader.load_data()
+        logging.info(f"Gmail Loader: {len(documents)} mails found")
 
         output = []
         for document in documents:
-            with open("out", "w") as f:
-                f.write(document.text)
-            print(document)
+            original_size = len(document.text)
 
-            print(len(documents))
+            snippet = document.metadata.get("snippet")
+            meta_data = {
+                "url": document.metadata.get("id"),
+                "date": get_header(document.text, "Date"),
+                "subject": get_header(document.text, "Subject"),
+                "from": get_header(document.text, "From"),
+                "to": get_header(document.text, "To"),
+                "search_query": query,
+            }
 
-            id_start = document.text.find("Message-ID")
-            id_end = document.text.find("\n", id_start)
-            id = document.text[id_start + len("Message-ID") : id_end]
-            print(id)
+            # Decode
+            decoded_bytes = quopri.decodestring(document.text)
+            decoded_str = decoded_bytes.decode("utf-8", errors="replace")
 
-            data = document.text
+            # Slice
+            mail_start = decoded_str.find("<!DOCTYPE")
+            data = decoded_str[mail_start:]
+
+            # Web Page HTML Processing
             soup = BeautifulSoup(data, "html.parser")
-            original_size = len(str(soup.get_text()))
 
             tags_to_exclude = [
                 "nav",
@@ -77,7 +99,7 @@ class GmailLoader(BaseLoader):
                     f"[{id}] Cleaned page size: {cleaned_size} characters, down from {original_size} (shrunk: {original_size-cleaned_size} chars, {round((1-(cleaned_size/original_size)) * 100, 2)}%)"  # noqa:E501
                 )
 
-            output.append({"meta_data": {"url": id}, "content": content})
+            result = f"email from '{meta_data.get('from')}' to '{meta_data.get('to')}'\nsubject: {meta_data.get('subject')}\ndate: {meta_data.get('date')}\npreview: {snippet}\ncontent: f{content}\n"
+            output.append({"meta_data": meta_data, "content": result})
 
-        print(output)
         return output
