45e071d0ce2a26b8f254b50320476bca5484228d
Prashant <prashantp099@gmail.com>
2023-08-10 22:41:19 +0530

    fix: Using ElasticsearchDBConfig as es db config, updated documentation

--- embedchain/config/apps/CustomAppConfig.py
+++ embedchain/config/apps/CustomAppConfig.py
@@ -3,6 +3,7 @@ from typing import Any
 from chromadb.api.types import Documents, Embeddings
 from dotenv import load_dotenv
 
+from embedchain.config.vectordbs import ElasticsearchDBConfig
 from embedchain.models import EmbeddingFunctions, Providers, VectorDatabases, VectorDimensions
 
 from .BaseAppConfig import BaseAppConfig
@@ -29,6 +30,7 @@ class CustomAppConfig(BaseAppConfig):
         open_source_app_config=None,
         deployment_name=None,
         db_type: VectorDatabases = None,
+        es_config: ElasticsearchDBConfig = None,
     ):
         """
         :param log_level: Optional. (String) Debug level
@@ -43,6 +45,7 @@ class CustomAppConfig(BaseAppConfig):
         :param provider: Optional. (Providers): LLM Provider to use.
         :param open_source_app_config: Optional. Config instance needed for open source apps.
         :param db_type: Optional. type of Vector database to use.
+        :param es_config: Optional. elasticsearch database config to be used for connection
         """
         if provider:
             self.provider = provider
@@ -63,6 +66,7 @@ class CustomAppConfig(BaseAppConfig):
             collection_name=collection_name,
             db_type=db_type,
             vector_dim=CustomAppConfig.get_vector_dimension(embedding_function=embedding_fn),
+            es_config=es_config,
         )
 
     @staticmethod
--- tests/vectordb/test_elasticsearch_db.py
+++ tests/vectordb/test_elasticsearch_db.py
@@ -1,33 +1,33 @@
 import unittest
-from unittest.mock import MagicMock, Mock, patch
+from unittest.mock import Mock
 
+from embedchain.config import ElasticsearchDBConfig
 from embedchain.vectordb.elasticsearch_db import ElasticsearchDB
 
 
 class TestEsDB(unittest.TestCase):
     def setUp(self):
-        # set mock es client
-        self.mock_client = MagicMock()
-        self.mock_client.indices.exists.return_value = True
+        self.es_config = ElasticsearchDBConfig()
+        self.vector_dim = 384
 
     def test_init_with_invalid_embedding_fn(self):
         # Test if an exception is raised when an invalid embedding_fn is provided
         with self.assertRaises(ValueError):
             ElasticsearchDB(embedding_fn=None)
 
+    def test_init_with_invalid_es_config(self):
+        # Test if an exception is raised when an invalid es_config is provided
+        with self.assertRaises(ValueError):
+            ElasticsearchDB(embedding_fn=Mock(), es_config=None)
+
     def test_init_with_invalid_vector_dim(self):
         # Test if an exception is raised when an invalid vector_dim is provided
         with self.assertRaises(ValueError):
-            ElasticsearchDB(embedding_fn=Mock(), es_client=self.mock_client, vector_dim=None)
+            ElasticsearchDB(embedding_fn=Mock(), es_config=self.es_config, vector_dim=None)
 
-    def test_init_with_valid_embedding_and_client(self):
-        # check for successful creation of ElasticsearchDB instance
-        esdb = ElasticsearchDB(embedding_fn=Mock(), es_client=self.mock_client, vector_dim=1024)
-        self.assertIsInstance(esdb, ElasticsearchDB)
-
-    @patch("os.getenv")  # Mock the os.getenv function to return None for ES_ENDPOINT
-    def test_init_with_missing_endpoint(self, mock_os_getenv):
-        # Test if an exception is raised when ES_ENDPOINT is missing
-        mock_os_getenv.return_value = None
+    def test_init_with_invalid_collection_name(self):
+        # Test if an exception is raised when an invalid collection_name is provided
         with self.assertRaises(ValueError):
-            ElasticsearchDB(embedding_fn=Mock())
+            ElasticsearchDB(
+                embedding_fn=Mock(), es_config=self.es_config, vector_dim=self.vector_dim, collection_name=None
+            )
