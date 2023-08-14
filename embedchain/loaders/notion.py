c26559a2d3418576d0cf36accf36384c3567fd20
cachho <admin@ch-webdev.com>
2023-08-14 23:27:23 +0200

    fix: notion install error message (#439)

--- embedchain/loaders/notion.py
+++ embedchain/loaders/notion.py
@@ -4,7 +4,7 @@ import os
 try:
     from llama_index import download_loader
 except ImportError:
-    raise ImportError("Notion requires extra dependencies. Install with `pip install embedchain[notion]`") from None
+    raise ImportError("Notion requires extra dependencies. Install with `pip install embedchain[community]`") from None
 
 
 from embedchain.loaders.base_loader import BaseLoader
