bfb9fd6310052596120e26e00999f23a39e8c242
cachho <admin@ch-webdev.com>
2023-08-18 19:27:02 +0200

    feat: language detection

--- embedchain/chunkers/repo_chunker.py
+++ embedchain/chunkers/repo_chunker.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from langchain.text_splitter import Language, RecursiveCharacterTextSplitter
 from whats_that_code.election import guess_language_all_methods
+import logging
 
 from embedchain.chunkers.base_chunker import BaseChunker
 from embedchain.config.AddConfig import ChunkerConfig
@@ -20,8 +21,18 @@ class RepoChunker(BaseChunker):
     def _set_dynamic_text_splitter(self, content, metadata):
         filename = metadata.get('filename')
 
-        self.text_splitter = RecursiveCharacterTextSplitter.from_language(
-            language=RepoChunker._get_detected_language(filename, content),
+        detected_language = RepoChunker._get_detected_language(filename, content)
+
+        if detected_language:
+            self.text_splitter = RecursiveCharacterTextSplitter.from_language(
+                language=detected_language,
+                chunk_size=self.config.chunk_size,
+                chunk_overlap=self.config.chunk_overlap,
+                length_function=self.config.length_function,
+            )
+        else:
+            # Fallback if filetype could not be detected.
+            self.text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=self.config.chunk_size,
             chunk_overlap=self.config.chunk_overlap,
             length_function=self.config.length_function,
@@ -30,5 +41,15 @@ class RepoChunker(BaseChunker):
     @staticmethod
     def _get_detected_language(filename, code) -> Language:
         detected = guess_language_all_methods(code, file_name=filename)
-        print("Detected:", detected)
+        logging.debug(f"Detected `{filename}` as `{detected}`")
+
+        # Rewrites
+        if detected == "javascript":
+            detected = Language.JS
+
+        if detected not in [language.value for language in Language]:
+            logging.debug(f"Detected language `{detected}` is not a compatibe language. Falling back to plaintext text splitter.")
+            return None
+        
+
         return detected
