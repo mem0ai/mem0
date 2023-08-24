683c1770596ebfdc89fb75f6628d2da5e0c6b3f3
cachho <admin@ch-webdev.com>
2023-08-24 23:23:25 +0200

    fix: dependencies (#416)

--- embedchain/config/apps/OpenSourceAppConfig.py
+++ embedchain/config/apps/OpenSourceAppConfig.py
@@ -50,4 +50,10 @@ class OpenSourceAppConfig(BaseAppConfig):
 
         :returns: The default embedding function
         """
-        return embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
+        try:
+            return embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
+        except ValueError as e:
+            print(e)
+            raise ModuleNotFoundError(
+                "The open source app requires extra dependencies. Install with `pip install embedchain[opensource]`"
+            ) from None
