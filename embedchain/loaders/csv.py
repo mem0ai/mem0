f3068ab029a3f3c40a907635fa0696daec1c9639
cachho <admin@ch-webdev.com>
2023-08-24 16:28:15 +0200

    refactor: use static method

--- embedchain/loaders/csv.py
+++ embedchain/loaders/csv.py
@@ -8,12 +8,14 @@ from embedchain.loaders.base_loader import BaseLoader
 
 
 class CsvLoader(BaseLoader):
-    def detect_delimiter(self, first_line):
+    @staticmethod
+    def detect_delimiter(first_line):
         delimiters = [",", "\t", ";", "|"]
         counts = {delimiter: first_line.count(delimiter) for delimiter in delimiters}
         return max(counts, key=counts.get)
 
-    def get_file_content(self, content):
+    @staticmethod
+    def get_file_content(content):
         url = urlparse(content)
         if all([url.scheme, url.netloc]) and url.scheme not in ["file", "http", "https"]:
             raise ValueError("Not a valid URL.")
@@ -28,13 +30,14 @@ class CsvLoader(BaseLoader):
         else:
             return open(content, newline="")  # Treat content as a regular file path
 
-    def load_data(self, content):
+    @staticmethod
+    def load_data(content):
         """Load a csv file with headers. Each line is a document"""
         result = []
 
-        with self.get_file_content(content) as file:
+        with CsvLoader.get_file_content(content) as file:
             first_line = file.readline()
-            delimiter = self.detect_delimiter(first_line)
+            delimiter = CsvLoader.detect_delimiter(first_line)
             file.seek(0)  # Reset the file pointer to the start
             reader = csv.DictReader(file, delimiter=delimiter)
             for i, row in enumerate(reader):
