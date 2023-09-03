8d97430b65266922b8b9bd7d7c28af185d161222
cachho <admin@ch-webdev.com>
2023-09-03 13:52:06 +0200

    fix: circular dependency

--- embedchain/embedder/base_embedder.py
+++ embedchain/embedder/base_embedder.py
@@ -1,6 +1,6 @@
 from typing import Any, Callable
 
-from embedchain.config import BaseEmbedderConfig
+from embedchain.config.embedder.BaseEmbedderConfig import BaseEmbedderConfig
 
 try:
     from chromadb.api.types import Documents, Embeddings
