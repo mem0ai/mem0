b68e3ac0245d0eafa006375c05f887c4793c7eee
cachho <admin@ch-webdev.com>
2023-09-03 19:34:44 +0200

    refactor: made llama2 a custom app

--- embedchain/apps/Llama2App.py
+++ embedchain/apps/Llama2App.py
@@ -2,11 +2,14 @@ import os
 
 from langchain.llms import Replicate
 
-from embedchain.config import AppConfig, BaseLlmConfig
-from embedchain.embedchain import EmbedChain
+from embedchain.config import AppConfig, BaseLlmConfig, CustomAppConfig
+from embedchain.apps.CustomApp import CustomApp
+from embedchain.llm.llama2_llm import Llama2Llm
+from embedchain.vectordb.chroma_db import ChromaDB
+from embedchain.embedder.openai_embedder import OpenAiEmbedder
 
 
-class Llama2App(EmbedChain):
+class Llama2App(CustomApp):
     """
     The EmbedChain Llama2App class.
     Has two functions: add and query.
@@ -15,24 +18,13 @@ class Llama2App(EmbedChain):
     query(query): finds answer to the given query using vector database and LLM.
     """
 
-    def __init__(self, config: AppConfig = None):
+    def __init__(self, config: CustomAppConfig = None):
         """
         :param config: AppConfig instance to load as configuration. Optional.
         """
-        if "REPLICATE_API_TOKEN" not in os.environ:
-            raise ValueError("Please set the REPLICATE_API_TOKEN environment variable.")
 
         if config is None:
             config = AppConfig()
 
-        super().__init__(config)
+        super().__init__(config=config, llm=Llama2Llm(), db=ChromaDB(), embedder=OpenAiEmbedder())
 
-    def get_llm_model_answer(self, prompt, config: BaseLlmConfig = None):
-        # TODO: Move the model and other inputs into config
-        if config.system_prompt:
-            raise ValueError("Llama2App does not support `system_prompt`")
-        llm = Replicate(
-            model="a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",
-            input={"temperature": 0.75, "max_length": 500, "top_p": 1},
-        )
-        return llm(prompt)
