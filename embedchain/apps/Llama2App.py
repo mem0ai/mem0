7e7516ffb0694131636a2515c34fc8481243ce92
cachho <admin@ch-webdev.com>
2023-09-04 12:52:12 +0200

    fix: use CustomAppConfig

--- embedchain/apps/Llama2App.py
+++ embedchain/apps/Llama2App.py
@@ -1,11 +1,7 @@
-import os
 from typing import Optional
 
-from langchain.llms import Replicate
-
 from embedchain.apps.CustomApp import CustomApp
-from embedchain.config import AppConfig, ChatConfig, CustomAppConfig
-from embedchain.embedchain import EmbedChain
+from embedchain.config import CustomAppConfig
 from embedchain.embedder.openai_embedder import OpenAiEmbedder
 from embedchain.llm.llama2_llm import Llama2Llm
 from embedchain.vectordb.chroma_db import ChromaDB
@@ -20,14 +16,14 @@ class Llama2App(CustomApp):
     query(query): finds answer to the given query using vector database and LLM.
     """
 
-    def __init__(self, config: AppConfig = None, system_prompt: Optional[str] = None):
+    def __init__(self, config: CustomAppConfig = None, system_prompt: Optional[str] = None):
         """
-        :param config: AppConfig instance to load as configuration. Optional.
+        :param config: CustomAppConfig instance to load as configuration. Optional.
         :param system_prompt: System prompt string. Optional.
         """
 
         if config is None:
-            config = AppConfig()
+            config = CustomAppConfig()
 
         super().__init__(
             config=config, llm=Llama2Llm(), db=ChromaDB(), embedder=OpenAiEmbedder(), system_prompt=system_prompt
