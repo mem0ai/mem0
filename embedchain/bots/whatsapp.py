4dc02138b67214e732583a002532c5fad7bc85ad
cachho <admin@ch-webdev.com>
2023-09-05 23:30:51 +0200

    fix: typo

--- embedchain/bots/whatsapp.py
+++ embedchain/bots/whatsapp.py
@@ -17,7 +17,7 @@ class WhatsAppBot(BaseBot):
             self.twilio = importlib.import_module("twilio")
         except ModuleNotFoundError:
             raise ModuleNotFoundError(
-                "The required dependencies for Whatsapp are not installed."
+                "The required dependencies for Whatsapp are not installed. "
                 'Please install with `pip install "embedchain[whatsapp]"`'
             ) from None
         super().__init__()
