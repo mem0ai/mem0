47bc2858a08cdc22e37d6467e095493c9f593628
Deshraj Yadav <deshrajdry@gmail.com>
2023-09-09 22:20:24 -0700

    Remove elasticsearch as mandatory dependency (#585)

--- embedchain/__init__.py
+++ embedchain/__init__.py
@@ -6,7 +6,5 @@ from embedchain.apps.App import App  # noqa: F401
 from embedchain.apps.CustomApp import CustomApp  # noqa: F401
 from embedchain.apps.Llama2App import Llama2App  # noqa: F401
 from embedchain.apps.OpenSourceApp import OpenSourceApp  # noqa: F401
-from embedchain.apps.PersonApp import (PersonApp,  # noqa: F401
-                                       PersonOpenSourceApp)
+from embedchain.apps.PersonApp import PersonApp, PersonOpenSourceApp  # noqa: F401
 from embedchain.vectordb.chroma_db import ChromaDB  # noqa: F401
-from embedchain.vectordb.elasticsearch_db import ElasticsearchDB  # noqa: F401
