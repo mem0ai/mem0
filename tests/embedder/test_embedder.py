3ecbc86dfc727255cbfd9e92821cc7a15458e29d
cachho <admin@ch-webdev.com>
2023-09-12 13:24:58 +0200

    chore: linting

--- tests/embedder/test_embedder.py
+++ tests/embedder/test_embedder.py
@@ -2,6 +2,7 @@ import unittest
 
 from embedchain.embedder.base import BaseEmbedder
 
+
 class TestEmbedder(unittest.TestCase):
     def test_init_with_invalid_vector_dim(self):
         # Test if an exception is raised when an invalid vector_dim is provided
