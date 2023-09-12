3f99047429bd2492307784a6f517f2947fdeffcf
cachho <admin@ch-webdev.com>
2023-09-12 13:34:32 +0200

    test: test without url

--- tests/vectordb/test_elasticsearch_db.py
+++ tests/vectordb/test_elasticsearch_db.py
@@ -1,7 +1,7 @@
 import unittest
+import os
 
 from embedchain.config import ElasticsearchDBConfig
-from embedchain.embedder.base import BaseEmbedder
 from embedchain.vectordb.elasticsearch import ElasticsearchDB
 
 
@@ -10,10 +10,16 @@ class TestEsDB(unittest.TestCase):
         self.es_config = ElasticsearchDBConfig()
         self.vector_dim = 384
 
+    def test_init_without_url(self):
+        del os.environ["ELASTICSEARCH_URL"]
+        # Test if an exception is raised when an invalid es_config is provided
+        with self.assertRaises(AttributeError):
+            ElasticsearchDB()
+
     def test_init_with_invalid_es_config(self):
         # Test if an exception is raised when an invalid es_config is provided
         with self.assertRaises(ValueError):
-            ElasticsearchDB(es_config=None)
+            ElasticsearchDB(es_config={"valid es_config": False})
 
     def test_init_with_invalid_collection_name(self):
         # Test if an exception is raised when an invalid collection_name is provided
