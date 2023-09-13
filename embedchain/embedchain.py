5e06a0d00128c5217e89990b84a1d17b7633bd7f
David Talson <37641675+dtee1@users.noreply.github.com>
2023-09-13 16:36:23 -0400

    Fix/dont print the entire text when data type is text (#605)

--- embedchain/embedchain.py
+++ embedchain/embedchain.py
@@ -284,7 +284,10 @@ class EmbedChain(JSONSerializable):
             data_dict = {id: value for id, value in data_dict.items() if id not in existing_ids}
 
             if not data_dict:
-                print(f"All data from {src} already exists in the database.")
+                src_copy = src
+                if len(src_copy) > 50:
+                    src_copy = src[:50] + "..."
+                print(f"All data from {src_copy} already exists in the database.")
                 # Make sure to return a matching return type
                 return [], [], [], 0
 
@@ -418,7 +421,10 @@ class EmbedChain(JSONSerializable):
             data_dict = {id: value for id, value in data_dict.items() if id not in existing_ids}
 
             if not data_dict:
-                print(f"All data from {src} already exists in the database.")
+                src_copy = src
+                if len(src_copy) > 50:
+                    src_copy = src[:50] + "..."
+                print(f"All data from {src_copy} already exists in the database.")
                 # Make sure to return a matching return type
                 return [], [], [], 0
 
