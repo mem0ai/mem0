6c127d9e66cee319580cc1dcff586a198c98f83a
cachho <admin@ch-webdev.com>
2023-09-13 13:06:10 +0200

    chore: explicitly declare datatype

--- embedchain/models/data_type.py
+++ embedchain/models/data_type.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+
 class DirectDataType(Enum):
     """
     DirectDataType enum contains data types that contain raw data directly.
@@ -26,17 +27,21 @@ class IndirectDataType(Enum):
 
 class SpecialDataType(Enum):
     """
-    SpeciallDataType enum contains data types that are neither direct or indirect, or simply require special attention.
+    SpecialDataType enum contains data types that are neither direct nor indirect, or simply require special attention.
     """
 
     QNA_PAIR = "qna_pair"
 
 
-DataType = Enum(
-    "DataType",
-    {
-        **{item.name: item.value for item in DirectDataType},
-        **{item.name: item.value for item in IndirectDataType},
-        **{item.name: item.value for item in SpecialDataType},
-    },
-)
+class DataType(Enum):
+    TEXT = DirectDataType.TEXT.value
+    YOUTUBE_VIDEO = IndirectDataType.YOUTUBE_VIDEO.value
+    PDF_FILE = IndirectDataType.PDF_FILE.value
+    WEB_PAGE = IndirectDataType.WEB_PAGE.value
+    SITEMAP = IndirectDataType.SITEMAP.value
+    DOCX = IndirectDataType.DOCX.value
+    DOCS_SITE = IndirectDataType.DOCS_SITE.value
+    NOTION = IndirectDataType.NOTION.value
+    CSV = IndirectDataType.CSV.value
+    MDX = IndirectDataType.MDX.value
+    QNA_PAIR = SpecialDataType.QNA_PAIR.value
