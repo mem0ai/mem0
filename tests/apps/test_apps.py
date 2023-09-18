893b6168c64c58c8986581082a30b1b190d6a58f
cachho <admin@ch-webdev.com>
2023-09-18 15:16:20 +0200

    test: config

--- tests/apps/test_apps.py
+++ tests/apps/test_apps.py
@@ -2,9 +2,11 @@ import os
 import unittest
 
 from embedchain import App, CustomApp, Llama2App, OpenSourceApp
+from embedchain.config import ChromaDbConfig
 from embedchain.embedder.base import BaseEmbedder
 from embedchain.llm.base import BaseLlm
 from embedchain.vectordb.base import BaseVectorDB
+from embedchain.vectordb.chroma import ChromaDB
 
 
 class TestApps(unittest.TestCase):
@@ -37,3 +39,22 @@ class TestApps(unittest.TestCase):
         self.assertIsInstance(app.llm, BaseLlm)
         self.assertIsInstance(app.db, BaseVectorDB)
         self.assertIsInstance(app.embedder, BaseEmbedder)
+
+
+class TestConfigForAppComponents(unittest.TestCase):
+    collection_name = "my-test-collection"
+
+    def test_constructor_config(self):
+        """
+        Test that app can be configured through the app constructor.
+        """
+        app = App(db_config=ChromaDbConfig(collection_name=self.collection_name))
+        self.assertEqual(app.db.config.collection_name, self.collection_name)
+
+    def test_component_config(self):
+        """
+        Test that app can also be configured by passing a configured component to the app
+        """
+        database = ChromaDB(config=ChromaDbConfig(collection_name=self.collection_name))
+        app = App(db=database)
+        self.assertEqual(app.db.config.collection_name, self.collection_name)
