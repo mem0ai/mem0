15d5761f0b2ff533d729f767ac8a0a4636732347
cachho <admin@ch-webdev.com>
2023-09-18 12:44:53 +0200

    feat: fully configurable app

--- embedchain/apps/App.py
+++ embedchain/apps/App.py
@@ -1,5 +1,5 @@
 from typing import Optional
-
+import logging
 from embedchain.config import (AppConfig, BaseEmbedderConfig, BaseLlmConfig,
                                ChromaDbConfig)
 from embedchain.embedchain import EmbedChain
@@ -7,7 +7,10 @@ from embedchain.embedder.openai import OpenAiEmbedder
 from embedchain.helper.json_serializable import register_deserializable
 from embedchain.llm.openai import OpenAILlm
 from embedchain.vectordb.chroma import ChromaDB
-
+from embedchain.llm.base import BaseLlm
+from embedchain.vectordb.base import BaseVectorDB
+from embedchain.config.vectordbs.BaseVectorDbConfig import BaseVectorDbConfig
+from embedchain.embedder.base import BaseEmbedder
 
 @register_deserializable
 class App(EmbedChain):
@@ -23,13 +26,18 @@ class App(EmbedChain):
 
     def __init__(
         self,
-        config: AppConfig = None,
-        llm_config: BaseLlmConfig = None,
+        config: Optional[AppConfig] = None,
+        llm: BaseLlm = None,
+        llm_config: Optional[BaseLlmConfig] = None,
+        db: BaseVectorDB = None,
+        db_config: Optional[BaseVectorDbConfig] = None,
+        embedder: BaseEmbedder = None,
+        embedder_config: Optional[BaseEmbedderConfig] = None,
         chromadb_config: Optional[ChromaDbConfig] = None,
         system_prompt: Optional[str] = None,
     ):
         """
-        Initialize a new `CustomApp` instance. You only have a few choices to make.
+        Initialize a new `CustomApp` instance.
 
         :param config: Config for the app instance.
         This is the most basic configuration, that does not fall into the LLM, database or embedder category,
@@ -44,11 +52,58 @@ class App(EmbedChain):
         :param system_prompt: System prompt that will be provided to the LLM as such, defaults to None
         :type system_prompt: Optional[str], optional
         """
+        # Overwrite deprecated arguments
+        if chromadb_config:
+            logging.warning("DEPRECATION WARNING: Please use `db_config` argument instead of `chromadb_config`. `chromadb_config` will be removed in a future release.")
+            db_config = chromadb_config
+
+        # Check configs
+        if not isinstance(config, AppConfig):
+            raise TypeError(
+                "Config is not a `AppConfig` instance. "
+                "Please make sure the type is right and that you are passing an instance."
+            )
+        if llm_config and not isinstance(llm_config, BaseLlmConfig):
+            raise TypeError(
+                "`llm_config` is not a `BaseLlmConfig` instance. "
+                "Please make sure the type is right and that you are passing an instance."
+            )
+        if db_config and not isinstance(db_config, BaseVectorDbConfig):
+            raise TypeError(
+                "`db_config` is not a `BaseVectorDbConfig` instance. "
+                "Please make sure the type is right and that you are passing an instance."
+            )
+        if embedder_config and not isinstance(embedder_config, BaseEmbedderConfig):
+            raise TypeError(
+                "`embedder_config` is not a `BaseEmbedderConfig` instance. "
+                "Please make sure the type is right and that you are passing an instance."
+            )
+        
+        # Assign defaults
         if config is None:
             config = AppConfig()
+        if llm is None:
+            llm = OpenAILlm(config=llm_config)
+        if db is None:
+            db = ChromaDB(config=db_config)
+        if embedder is None:
+            embedder = OpenAiEmbedder(config=embedder_config)
 
-        llm = OpenAILlm(config=llm_config)
-        embedder = OpenAiEmbedder(config=BaseEmbedderConfig(model="text-embedding-ada-002"))
-        database = ChromaDB(config=chromadb_config)
+        # Type check assignments
+        if not isinstance(llm, BaseLlm):
+            raise TypeError(
+                "LLM is not a `BaseLlm` instance. "
+                "Please make sure the type is right and that you are passing an instance."
+            )
+        if not isinstance(db, BaseVectorDB):
+            raise TypeError(
+                "Database is not a `BaseVectorDB` instance. "
+                "Please make sure the type is right and that you are passing an instance."
+            )
+        if not isinstance(embedder, BaseEmbedder):
+            raise TypeError(
+                "Embedder is not a `BaseEmbedder` instance. "
+                "Please make sure the type is right and that you are passing an instance."
+            )
 
-        super().__init__(config, llm, db=database, embedder=embedder, system_prompt=system_prompt)
+        super().__init__(config, llm=llm, db=db, embedder=embedder, system_prompt=system_prompt)
