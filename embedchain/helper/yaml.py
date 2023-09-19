05f403ae2c82d822c0f484792ca023044cde7d7c
cachho <admin@ch-webdev.com>
2023-09-19 15:54:04 +0200

    feat: generate default config

--- embedchain/helper/yaml.py
+++ embedchain/helper/yaml.py
@@ -1,6 +1,7 @@
 import json
-from typing import Dict
 import logging
+from typing import Dict
+
 import yaml
 
 
@@ -22,32 +23,32 @@ def sanitize(data: Dict[str, str]) -> Dict[str, str]:
     del data["u_id"]
 
     # If there's only one kind of config, there's no need to name the class
-    del data['llm']['config']['__class__']
-    del data['embedder']['config']['__class__']
+    del data["llm"]["config"]["__class__"]
+    del data["embedder"]["config"]["__class__"]
     # db has separate configs, so the class matters.
 
     # Database references the sibling embedder class. This can be removed
-    del data['db']['embedder']
+    del data["db"]["embedder"]
 
     # Collection is a database attribute. The app attribute is deprecated.
-    del data['config']['collection_name']
+    del data["config"]["collection_name"]
 
     # There's only one app type.
-    del data['__class__']
+    del data["__class__"]
 
     # All remaining attributes are moved into 'session'
     keys_to_delete = []
-    data['session'] = {}
+    data["session"] = {}
     for key, value in data.items():
         if not isinstance(value, dict):
-            data['session'][key] = value
+            data["session"][key] = value
             keys_to_delete.append(key)
     for key in keys_to_delete:
         del data[key]
 
     # Delete session if it remained empty
-    if data['session'] == {}:
-        del data['session']
+    if data["session"] == {}:
+        del data["session"]
 
     # Return dict
     return data
@@ -64,3 +65,15 @@ class Yaml:
             yaml.dump(sanitized_data, file, default_flow_style=False)
 
         logging.info(f"Saved config to {filename}")
+
+    @staticmethod
+    def generate_default_config():
+        """
+        Generate a default yaml config in your file system,
+        which you can adapt to your needs.
+
+        example: `from embedchain import App; App.generate_default_config()`
+        """
+        from embedchain import App
+
+        App().save()
