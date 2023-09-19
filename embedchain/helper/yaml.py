c1a97e6c92131042ef14643623164fad3f90b2e7
cachho <admin@ch-webdev.com>
2023-09-19 16:24:09 +0200

    chore: turned into static methods

--- embedchain/helper/yaml.py
+++ embedchain/helper/yaml.py
@@ -7,74 +7,6 @@ import yaml
 from embedchain.helper.json_serializable import JSONSerializable
 
 
-def sanitize(data: Dict[str, str]) -> Dict[str, str]:
-    """
-    Optimize for human use by removing parts that are:
-    1. duplicates
-    2. hard to read
-    3. redundant
-
-    :param data: serialized json data
-    :type data: Dict[str, str]
-    :return: sanitized serialized json data
-    :rtype: Dict[str, str]
-    """
-    # Session id should not be stored persistently
-    del data["s_id"]
-    # unique id is already stored persistently
-    del data["u_id"]
-
-    # If there's only one kind of config, there's no need to name the class
-    del data["llm"]["config"]["__class__"]
-    del data["embedder"]["config"]["__class__"]
-    # db has separate configs, so the class matters.
-
-    # Database references the sibling embedder class. This can be removed
-    del data["db"]["embedder"]
-
-    # Collection is a database attribute. The app attribute is deprecated.
-    del data["config"]["collection_name"]
-
-    # There's only one app type.
-    del data["__class__"]
-
-    # All remaining attributes are moved into 'session'
-    keys_to_delete = []
-    data["session"] = {}
-    for key, value in data.items():
-        if not isinstance(value, dict):
-            data["session"][key] = value
-            keys_to_delete.append(key)
-    for key in keys_to_delete:
-        del data[key]
-
-    # Delete session if it remained empty
-    if data["session"] == {}:
-        del data["session"]
-
-    # Return dict
-    return data
-
-
-def desanitize(data: Dict[str, str]) -> Dict[str, str]:
-    # Move attributes from 'session' back to the top level
-    session_data = data.pop("session", {})
-    for key, value in session_data.items():
-        data[key] = value
-
-    # Recreate basic class
-    data["__class__"] = "AppConfig"
-
-    # DB Embedder = Embedder
-    data["db"]["embedder"] = data["embedder"]
-
-    # Restore BaseConfigNames
-    data["llm"]["config"]["__class__"] = "BaseLlmConfig"
-    data["embedder"]["config"]["__class__"] = "BaseEmbedderConfig"
-
-    return data
-
-
 class Yaml(JSONSerializable):
     def save(self, filename: str = "config.yaml"):
         """
@@ -86,7 +18,7 @@ class Yaml(JSONSerializable):
         data = json.loads(self.serialize())
 
         # Sanitize
-        sanitized_data = sanitize(data)
+        sanitized_data = Yaml.sanitize(data)
 
         with open(filename, "w") as file:
             yaml.dump(sanitized_data, file, default_flow_style=False)
@@ -116,10 +48,78 @@ class Yaml(JSONSerializable):
             data = yaml.safe_load(file)
 
         # Desanitize
-        desanitized_data = desanitize(data)
+        desanitized_data = Yaml.desanitize(data)
 
         # Convert dictionary back to a string to leverage App.deserialize
         data_str = json.dumps(desanitized_data)
 
         # Deserialize in-place
         self.deserialize_in_place(data_str)
+
+    @staticmethod
+    def sanitize(data: Dict[str, str]) -> Dict[str, str]:
+        """
+        Optimize for human use by removing parts that are:
+        1. duplicates
+        2. hard to read
+        3. redundant
+
+        :param data: serialized json data
+        :type data: Dict[str, str]
+        :return: sanitized serialized json data
+        :rtype: Dict[str, str]
+        """
+        # Session id should not be stored persistently
+        del data["s_id"]
+        # unique id is already stored persistently
+        del data["u_id"]
+
+        # If there's only one kind of config, there's no need to name the class
+        del data["llm"]["config"]["__class__"]
+        del data["embedder"]["config"]["__class__"]
+        # db has separate configs, so the class matters.
+
+        # Database references the sibling embedder class. This can be removed
+        del data["db"]["embedder"]
+
+        # Collection is a database attribute. The app attribute is deprecated.
+        del data["config"]["collection_name"]
+
+        # There's only one app type.
+        del data["__class__"]
+
+        # All remaining attributes are moved into 'session'
+        keys_to_delete = []
+        data["session"] = {}
+        for key, value in data.items():
+            if not isinstance(value, dict):
+                data["session"][key] = value
+                keys_to_delete.append(key)
+        for key in keys_to_delete:
+            del data[key]
+
+        # Delete session if it remained empty
+        if data["session"] == {}:
+            del data["session"]
+
+        # Return dict
+        return data
+
+    @staticmethod
+    def desanitize(data: Dict[str, str]) -> Dict[str, str]:
+        # Move attributes from 'session' back to the top level
+        session_data = data.pop("session", {})
+        for key, value in session_data.items():
+            data[key] = value
+
+        # Recreate basic class
+        data["__class__"] = "AppConfig"
+
+        # DB Embedder = Embedder
+        data["db"]["embedder"] = data["embedder"]
+
+        # Restore BaseConfigNames
+        data["llm"]["config"]["__class__"] = "BaseLlmConfig"
+        data["embedder"]["config"]["__class__"] = "BaseEmbedderConfig"
+
+        return data
