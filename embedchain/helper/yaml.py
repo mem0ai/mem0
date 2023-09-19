cfc3137894f9823561c441f3ad94785a256b4262
cachho <admin@ch-webdev.com>
2023-09-19 15:48:01 +0200

    feat: refined sanitation

--- embedchain/helper/yaml.py
+++ embedchain/helper/yaml.py
@@ -1,6 +1,6 @@
 import json
 from typing import Dict
-
+import logging
 import yaml
 
 
@@ -18,12 +18,38 @@ def sanitize(data: Dict[str, str]) -> Dict[str, str]:
     """
     # Session id should not be stored persistently
     del data["s_id"]
+    # unique id is already stored persistently
+    del data["u_id"]
 
-    # For most users, history should not be stored persistently.
-    # I'm sure there are use-cases for this.
-    if not data["llm"]["history"] or len(data["llm"]["history"]) == 0:
-        del data["llm"]["history"]
+    # If there's only one kind of config, there's no need to name the class
+    del data['llm']['config']['__class__']
+    del data['embedder']['config']['__class__']
+    # db has separate configs, so the class matters.
 
+    # Database references the sibling embedder class. This can be removed
+    del data['db']['embedder']
+
+    # Collection is a database attribute. The app attribute is deprecated.
+    del data['config']['collection_name']
+
+    # There's only one app type.
+    del data['__class__']
+
+    # All remaining attributes are moved into 'session'
+    keys_to_delete = []
+    data['session'] = {}
+    for key, value in data.items():
+        if not isinstance(value, dict):
+            data['session'][key] = value
+            keys_to_delete.append(key)
+    for key in keys_to_delete:
+        del data[key]
+
+    # Delete session if it remained empty
+    if data['session'] == {}:
+        del data['session']
+
+    # Return dict
     return data
 
 
@@ -36,3 +62,5 @@ class Yaml:
 
         with open(filename, "w") as file:
             yaml.dump(sanitized_data, file, default_flow_style=False)
+
+        logging.info(f"Saved config to {filename}")
