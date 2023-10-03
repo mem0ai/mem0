6d1e4fd82802d97399c79865255bd1e3bbb54a22
SerSamgy <samgy7cool@gmail.com>
2023-10-03 20:31:43 +0200

    F700: add full coverage to docs_site_loader

--- tests/loaders/test_docs_site_loader.py
+++ tests/loaders/test_docs_site_loader.py
@@ -26,48 +26,56 @@ from bs4 import BeautifulSoup
 <article class="bd-article">
     <h2>Article Title</h2>
     <p>Article content goes here.</p>
+    {ignored_tag}
 </article>
 """,
         """
 <article role="main">
     <h2>Main Article Title</h2>
     <p>Main article content goes here.</p>
+    {ignored_tag}
 </article>
 """,
         """
 <div class="md-content">
     <h2>Markdown Content</h2>
     <p>Markdown content goes here.</p>
+    {ignored_tag}
 </div>
 """,
         """
 <div role="main">
     <h2>Main Content</h2>
     <p>Main content goes here.</p>
+    {ignored_tag}
 </div>
 """,
         """
 <div class="container">
     <h2>Container</h2>
     <p>Container content goes here.</p>
+    {ignored_tag}
 </div>
         """,
         """
 <div class="section">
     <h2>Section</h2>
     <p>Section content goes here.</p>
+    {ignored_tag}
 </div>
         """,
         """
 <article>
     <h2>Generic Article</h2>
     <p>Generic article content goes here.</p>
+    {ignored_tag}
 </article>
         """,
         """
 <main>
     <h2>Main Content</h2>
     <p>Main content goes here.</p>
+    {ignored_tag}
 </main>
 """,
     ],
@@ -84,16 +92,16 @@ from bs4 import BeautifulSoup
 )
 def test_load_data_gets_by_selectors_and_ignored_tags(selectee, ignored_tag, loader, mocked_responses, mocker):
     child_url = "https://docs.embedchain.ai/quickstart"
+    selectee = selectee.format(ignored_tag=ignored_tag)
     html_body = """
 <!DOCTYPE html>
 <html lang="en">
 <body>
-    {ignored_tag}
     {selectee}
 </body>
 </html>
 """
-    html_body = html_body.format(selectee=selectee, ignored_tag=ignored_tag)
+    html_body = html_body.format(selectee=selectee)
     mocked_responses.get(child_url, body=html_body, status=200, content_type="text/html")
 
     url = "https://docs.embedchain.ai/"
@@ -113,8 +121,7 @@ def test_load_data_gets_by_selectors_and_ignored_tags(selectee, ignored_tag, loa
 
     result = loader.load_data(url)
     selector_soup = BeautifulSoup(selectee, "html.parser")
-    soup = BeautifulSoup(selector_soup.get_text(), "html.parser")
-    expected_content = next(soup.stripped_strings).replace("\n", " ")
+    expected_content = " ".join((selector_soup.select_one("h2").get_text(), selector_soup.select_one("p").get_text()))
     assert result["doc_id"] == doc_id
     assert result["data"] == [
         {
@@ -174,7 +181,28 @@ def test_load_data_gets_child_links_recursively(loader, mocked_responses, mocker
     assert all(item in expected_data for item in result["data"])
 
 
-# TODO: Add tests for failure cases.
+def test_load_data_fails_to_fetch_website(loader, mocked_responses, mocker):
+    child_url = "https://docs.embedchain.ai/introduction"
+    mocked_responses.get(child_url, status=404)
+
+    url = "https://docs.embedchain.ai/"
+    html_body = """
+<!DOCTYPE html>
+<html lang="en">
+<body>
+    <li><a href="/introduction">Introduction</a></li>
+</body>
+</html>
+"""
+    mocked_responses.get(url, body=html_body, status=200, content_type="text/html")
+
+    mock_sha256 = mocker.patch("embedchain.loaders.docs_site_loader.hashlib.sha256")
+    doc_id = "mocked_hash"
+    mock_sha256.return_value.hexdigest.return_value = doc_id
+
+    result = loader.load_data(url)
+    assert result["doc_id"] is doc_id
+    assert result["data"] == []
 
 
 @pytest.fixture
