63291c5e3e95b078922cc3789622f4352560bcc3
Yusuf <71589435+Yusufibin@users.noreply.github.com>
2023-10-10 21:55:06 +0100

    Create gpt4free.py

---
+++ embedchain/llm/gpt4free.py
@@ -0,0 +1,32 @@
+import logging
+from typing import Optional
+
+from embedchain.config import BaseLlmConfig
+from embedchain.helper.json_serializable import register_deserializable
+from embedchain.llm.base import BaseLlm
+
+
+@register_deserializable
+class Gpt4free(BaseLlm):
+    def __init__(self, config: Optional[BaseLlmConfig] = None):
+        super().__init__(config=config)
+
+    def get_llm_model_answer(self, prompt):
+        return Gpt4free._get_answer(prompt=prompt, config=self.config)
+
+    @staticmethod
+    def _get_answer(prompt: str, config: BaseLlmConfig) -> str:
+        #from langchain.chat_models import ChatAnthropic
+        import g4f 
+        response = g4f.ChatCompletion.create(model="gpt-3.5-turbo",provider=g4f.Provider.Acytoo,messages=[{"role": "user", "content":prompt}],
+    stream=False)   
+      
+        """
+        chat = ChatAnthropic(temperature=config.temperature, model=config.model)
+
+        if config.max_tokens and config.max_tokens != 1000:
+            logging.warning("Config option `max_tokens` is not supported by this model.")
+        """
+        messages = BaseLlm._get_messages(prompt, system_prompt=config.system_prompt)
+        return response 
+        #return chat(messages).content
