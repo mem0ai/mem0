511de78594112e8ae67b32d92f066f98b151c6c2
Sidharth Mohanty <sidmohanty11@gmail.com>
2023-10-15 13:05:28 +0530

    add more tests and fix openai llm

--- tests/llm/test_openai.py
+++ tests/llm/test_openai.py
@@ -1,28 +1,24 @@
 import os
 import pytest
 from embedchain.config import BaseLlmConfig
-from embedchain.llm.jina import JinaLlm
+from embedchain.llm.openai import OpenAILlm
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 
 @pytest.fixture
 def config():
-    os.environ["JINACHAT_API_KEY"] = "test_api_key"
-    config = BaseLlmConfig(temperature=0.7, max_tokens=50, top_p=0.8, stream=False, system_prompt="System prompt")
+    os.environ["OPENAI_API_KEY"] = "test_api_key"
+    config = BaseLlmConfig(
+        temperature=0.7, max_tokens=50, top_p=0.8, stream=False, system_prompt="System prompt", model="gpt-3.5-turbo"
+    )
     yield config
-    os.environ.pop("JINACHAT_API_KEY")
-
-
-def test_init_raises_value_error_without_api_key(mocker):
-    mocker.patch.dict(os.environ, clear=True)
-    with pytest.raises(ValueError):
-        JinaLlm()
+    os.environ.pop("OPENAI_API_KEY")
 
 
 def test_get_llm_model_answer(config, mocker):
-    mocked_get_answer = mocker.patch("embedchain.llm.jina.JinaLlm._get_answer", return_value="Test answer")
+    mocked_get_answer = mocker.patch("embedchain.llm.openai.OpenAILlm._get_answer", return_value="Test answer")
 
-    llm = JinaLlm(config)
+    llm = OpenAILlm(config)
     answer = llm.get_llm_model_answer("Test query")
 
     assert answer == "Test answer"
@@ -31,9 +27,9 @@ def test_get_llm_model_answer(config, mocker):
 
 def test_get_llm_model_answer_with_system_prompt(config, mocker):
     config.system_prompt = "Custom system prompt"
-    mocked_get_answer = mocker.patch("embedchain.llm.jina.JinaLlm._get_answer", return_value="Test answer")
+    mocked_get_answer = mocker.patch("embedchain.llm.openai.OpenAILlm._get_answer", return_value="Test answer")
 
-    llm = JinaLlm(config)
+    llm = OpenAILlm(config)
     answer = llm.get_llm_model_answer("Test query")
 
     assert answer == "Test answer"
@@ -41,9 +37,9 @@ def test_get_llm_model_answer_with_system_prompt(config, mocker):
 
 
 def test_get_llm_model_answer_empty_prompt(config, mocker):
-    mocked_get_answer = mocker.patch("embedchain.llm.jina.JinaLlm._get_answer", return_value="Test answer")
+    mocked_get_answer = mocker.patch("embedchain.llm.openai.OpenAILlm._get_answer", return_value="Test answer")
 
-    llm = JinaLlm(config)
+    llm = OpenAILlm(config)
     answer = llm.get_llm_model_answer("")
 
     assert answer == "Test answer"
@@ -52,9 +48,9 @@ def test_get_llm_model_answer_empty_prompt(config, mocker):
 
 def test_get_llm_model_answer_with_streaming(config, mocker):
     config.stream = True
-    mocked_jinachat = mocker.patch("embedchain.llm.jina.JinaChat")
+    mocked_jinachat = mocker.patch("embedchain.llm.openai.ChatOpenAI")
 
-    llm = JinaLlm(config)
+    llm = OpenAILlm(config)
     llm.get_llm_model_answer("Test query")
 
     mocked_jinachat.assert_called_once()
@@ -64,12 +60,13 @@ def test_get_llm_model_answer_with_streaming(config, mocker):
 
 def test_get_llm_model_answer_without_system_prompt(config, mocker):
     config.system_prompt = None
-    mocked_jinachat = mocker.patch("embedchain.llm.jina.JinaChat")
+    mocked_jinachat = mocker.patch("embedchain.llm.openai.ChatOpenAI")
 
-    llm = JinaLlm(config)
+    llm = OpenAILlm(config)
     llm.get_llm_model_answer("Test query")
 
     mocked_jinachat.assert_called_once_with(
+        model=config.model,
         temperature=config.temperature,
         max_tokens=config.max_tokens,
         model_kwargs={"top_p": config.top_p},
