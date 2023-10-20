d18e533adfa521098b190c223acbb43c980aaed4
Deshraj Yadav <deshrajdry@gmail.com>
2023-10-19 17:46:15 -0700

    [Feature] Setup base for creating pipelines in embedchain (#834)

--- embedchain/__init__.py
+++ embedchain/__init__.py
@@ -3,4 +3,5 @@ import importlib.metadata
 __version__ = importlib.metadata.version(__package__ or __name__)
 
 from embedchain.apps.app import App  # noqa: F401
+from embedchain.pipeline import Pipeline  # noqa: F401
 from embedchain.vectordb.chroma import ChromaDB  # noqa: F401
--- embedchain/config/__init__.py
+++ embedchain/config/__init__.py
@@ -2,10 +2,12 @@
 
 from .add_config import AddConfig, ChunkerConfig
 from .apps.app_config import AppConfig
+from .pipeline_config import PipelineConfig
 from .base_config import BaseConfig
 from .embedder.base import BaseEmbedderConfig
 from .embedder.base import BaseEmbedderConfig as EmbedderConfig
 from .llm.base import BaseLlmConfig
+from .pipeline_config import PipelineConfig
 from .vectordb.chroma import ChromaDbConfig
 from .vectordb.elasticsearch import ElasticsearchDBConfig
 from .vectordb.opensearch import OpenSearchDBConfig
