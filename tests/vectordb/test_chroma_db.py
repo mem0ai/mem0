6ffe9140bcd652452e6f4f759907872a15ef639c
Sidharth Mohanty <sidmohanty11@gmail.com>
2023-10-25 22:52:20 +0530

    [Bug fix] `reset()` erases everything from db (#844)

--- tests/vectordb/test_chroma_db.py
+++ tests/vectordb/test_chroma_db.py
@@ -1,14 +1,36 @@
 import os
 import shutil
+import pytest
 from unittest.mock import patch
 
 import pytest
 from chromadb.config import Settings
-
 from embedchain import App
 from embedchain.config import AppConfig, ChromaDbConfig
 from embedchain.vectordb.chroma import ChromaDB
 
+os.environ["OPENAI_API_KEY"] = "test-api-key"
+
+
+@pytest.fixture
+def chroma_db():
+    return ChromaDB(config=ChromaDbConfig(host="test-host", port="1234"))
+
+
+@pytest.fixture
+def app_with_settings():
+    chroma_config = ChromaDbConfig(allow_reset=True, dir="test-db")
+    app_config = AppConfig(collect_metrics=False)
+    return App(config=app_config, db_config=chroma_config)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def cleanup_db():
+    yield
+    try:
+        shutil.rmtree("test-db")
+    except OSError as e:
+        print("Error: %s - %s." % (e.filename, e.strerror))
 os.environ["OPENAI_API_KEY"] = "test-api-key"
 
 
@@ -37,8 +59,21 @@ def test_chroma_db_init_with_host_and_port(chroma_db):
     settings = chroma_db.client.get_settings()
     assert settings.chroma_server_host == "test-host"
     assert settings.chroma_server_http_port == "1234"
+def test_chroma_db_init_with_host_and_port(chroma_db):
+    settings = chroma_db.client.get_settings()
+    assert settings.chroma_server_host == "test-host"
+    assert settings.chroma_server_http_port == "1234"
 
 
+def test_chroma_db_init_with_basic_auth():
+    chroma_config = {
+        "host": "test-host",
+        "port": "1234",
+        "chroma_settings": {
+            "chroma_client_auth_provider": "chromadb.auth.basic.BasicAuthClientProvider",
+            "chroma_client_auth_credentials": "admin:admin",
+        },
+    }
 def test_chroma_db_init_with_basic_auth():
     chroma_config = {
         "host": "test-host",
@@ -55,8 +90,21 @@ def test_chroma_db_init_with_basic_auth():
     assert settings.chroma_server_http_port == "1234"
     assert settings.chroma_client_auth_provider == chroma_config["chroma_settings"]["chroma_client_auth_provider"]
     assert settings.chroma_client_auth_credentials == chroma_config["chroma_settings"]["chroma_client_auth_credentials"]
+    db = ChromaDB(config=ChromaDbConfig(**chroma_config))
+    settings = db.client.get_settings()
+    assert settings.chroma_server_host == "test-host"
+    assert settings.chroma_server_http_port == "1234"
+    assert settings.chroma_client_auth_provider == chroma_config["chroma_settings"]["chroma_client_auth_provider"]
+    assert settings.chroma_client_auth_credentials == chroma_config["chroma_settings"]["chroma_client_auth_credentials"]
 
 
+@patch("embedchain.vectordb.chroma.chromadb.Client")
+def test_app_init_with_host_and_port(mock_client):
+    host = "test-host"
+    port = "1234"
+    config = AppConfig(collect_metrics=False)
+    db_config = ChromaDbConfig(host=host, port=port)
+    _app = App(config, db_config=db_config)
 @patch("embedchain.vectordb.chroma.chromadb.Client")
 def test_app_init_with_host_and_port(mock_client):
     host = "test-host"
@@ -68,8 +116,14 @@ def test_app_init_with_host_and_port(mock_client):
     called_settings: Settings = mock_client.call_args[0][0]
     assert called_settings.chroma_server_host == host
     assert called_settings.chroma_server_http_port == port
+    called_settings: Settings = mock_client.call_args[0][0]
+    assert called_settings.chroma_server_host == host
+    assert called_settings.chroma_server_http_port == port
 
 
+@patch("embedchain.vectordb.chroma.chromadb.Client")
+def test_app_init_with_host_and_port_none(mock_client):
+    _app = App(config=AppConfig(collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db"))
 @patch("embedchain.vectordb.chroma.chromadb.Client")
 def test_app_init_with_host_and_port_none(mock_client):
     _app = App(config=AppConfig(collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db"))
@@ -77,8 +131,18 @@ def test_app_init_with_host_and_port_none(mock_client):
     called_settings: Settings = mock_client.call_args[0][0]
     assert called_settings.chroma_server_host is None
     assert called_settings.chroma_server_http_port is None
+    called_settings: Settings = mock_client.call_args[0][0]
+    assert called_settings.chroma_server_host is None
+    assert called_settings.chroma_server_http_port is None
 
 
+def test_chroma_db_duplicates_throw_warning(caplog):
+    app = App(config=AppConfig(collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db"))
+    app.db.collection.add(embeddings=[[0, 0, 0]], ids=["0"])
+    app.db.collection.add(embeddings=[[0, 0, 0]], ids=["0"])
+    assert "Insert of existing embedding ID: 0" in caplog.text
+    assert "Add of existing embedding ID: 0" in caplog.text
+    app.db.reset()
 def test_chroma_db_duplicates_throw_warning(caplog):
     app = App(config=AppConfig(collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db"))
     app.db.collection.add(embeddings=[[0, 0, 0]], ids=["0"])
@@ -99,13 +163,31 @@ def test_chroma_db_duplicates_collections_no_warning(caplog):
     app.db.reset()
     app.set_collection_name("test_collection_1")
     app.db.reset()
+def test_chroma_db_duplicates_collections_no_warning(caplog):
+    app = App(config=AppConfig(collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db"))
+    app.set_collection_name("test_collection_1")
+    app.db.collection.add(embeddings=[[0, 0, 0]], ids=["0"])
+    app.set_collection_name("test_collection_2")
+    app.db.collection.add(embeddings=[[0, 0, 0]], ids=["0"])
+    assert "Insert of existing embedding ID: 0" not in caplog.text
+    assert "Add of existing embedding ID: 0" not in caplog.text
+    app.db.reset()
+    app.set_collection_name("test_collection_1")
+    app.db.reset()
 
 
 def test_chroma_db_collection_init_with_default_collection():
     app = App(config=AppConfig(collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db"))
     assert app.db.collection.name == "embedchain_store"
+def test_chroma_db_collection_init_with_default_collection():
+    app = App(config=AppConfig(collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db"))
+    assert app.db.collection.name == "embedchain_store"
 
 
+def test_chroma_db_collection_init_with_custom_collection():
+    app = App(config=AppConfig(collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db"))
+    app.set_collection_name(name="test_collection")
+    assert app.db.collection.name == "test_collection"
 def test_chroma_db_collection_init_with_custom_collection():
     app = App(config=AppConfig(collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db"))
     app.set_collection_name(name="test_collection")
@@ -116,8 +198,16 @@ def test_chroma_db_collection_set_collection_name():
     app = App(config=AppConfig(collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db"))
     app.set_collection_name("test_collection")
     assert app.db.collection.name == "test_collection"
+def test_chroma_db_collection_set_collection_name():
+    app = App(config=AppConfig(collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db"))
+    app.set_collection_name("test_collection")
+    assert app.db.collection.name == "test_collection"
 
 
+def test_chroma_db_collection_changes_encapsulated():
+    app = App(config=AppConfig(collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db"))
+    app.set_collection_name("test_collection_1")
+    assert app.db.count() == 0
 def test_chroma_db_collection_changes_encapsulated():
     app = App(config=AppConfig(collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db"))
     app.set_collection_name("test_collection_1")
@@ -125,9 +215,20 @@ def test_chroma_db_collection_changes_encapsulated():
 
     app.db.collection.add(embeddings=[0, 0, 0], ids=["0"])
     assert app.db.count() == 1
+    app.db.collection.add(embeddings=[0, 0, 0], ids=["0"])
+    assert app.db.count() == 1
 
     app.set_collection_name("test_collection_2")
     assert app.db.count() == 0
+    app.set_collection_name("test_collection_2")
+    assert app.db.count() == 0
+
+    app.db.collection.add(embeddings=[0, 0, 0], ids=["0"])
+    app.set_collection_name("test_collection_1")
+    assert app.db.count() == 1
+    app.db.reset()
+    app.set_collection_name("test_collection_2")
+    app.db.reset()
 
     app.db.collection.add(embeddings=[0, 0, 0], ids=["0"])
     app.set_collection_name("test_collection_1")
@@ -140,9 +241,22 @@ def test_chroma_db_collection_changes_encapsulated():
 def test_chroma_db_collection_add_with_skip_embedding(app_with_settings):
     # Start with a clean app
     app_with_settings.db.reset()
+def test_chroma_db_collection_add_with_skip_embedding(app_with_settings):
+    # Start with a clean app
+    app_with_settings.db.reset()
 
     assert app_with_settings.db.count() == 0
+    assert app_with_settings.db.count() == 0
 
+    app_with_settings.db.add(
+        embeddings=[[0, 0, 0]],
+        documents=["document"],
+        metadatas=[{"value": "somevalue"}],
+        ids=["id"],
+        skip_embedding=True,
+    )
+
+    assert app_with_settings.db.count() == 1
     app_with_settings.db.add(
         embeddings=[[0, 0, 0]],
         documents=["document"],
@@ -162,6 +276,21 @@ def test_chroma_db_collection_add_with_skip_embedding(app_with_settings):
     }
 
     assert data == expected_value
+    data = app_with_settings.db.get(["id"], limit=1)
+    expected_value = {
+        "documents": ["document"],
+        "embeddings": None,
+        "ids": ["id"],
+        "metadatas": [{"value": "somevalue"}],
+    }
+
+    assert data == expected_value
+
+    data = app_with_settings.db.query(input_query=[0, 0, 0], where={}, n_results=1, skip_embedding=True)
+    expected_value = ["document"]
+
+    assert data == expected_value
+    app_with_settings.db.reset()
 
     data = app_with_settings.db.query(input_query=[0, 0, 0], where={}, n_results=1, skip_embedding=True)
     expected_value = ["document"]
@@ -173,9 +302,23 @@ def test_chroma_db_collection_add_with_skip_embedding(app_with_settings):
 def test_chroma_db_collection_add_with_invalid_inputs(app_with_settings):
     # Start with a clean app
     app_with_settings.db.reset()
+def test_chroma_db_collection_add_with_invalid_inputs(app_with_settings):
+    # Start with a clean app
+    app_with_settings.db.reset()
 
     assert app_with_settings.db.count() == 0
+    assert app_with_settings.db.count() == 0
 
+    with pytest.raises(ValueError):
+        app_with_settings.db.add(
+            embeddings=[[0, 0, 0]],
+            documents=["document", "document2"],
+            metadatas=[{"value": "somevalue"}],
+            ids=["id"],
+            skip_embedding=True,
+        )
+
+    assert app_with_settings.db.count() == 0
     with pytest.raises(ValueError):
         app_with_settings.db.add(
             embeddings=[[0, 0, 0]],
@@ -195,7 +338,17 @@ def test_chroma_db_collection_add_with_invalid_inputs(app_with_settings):
             ids=["id"],
             skip_embedding=True,
         )
+    with pytest.raises(ValueError):
+        app_with_settings.db.add(
+            embeddings=None,
+            documents=["document", "document2"],
+            metadatas=[{"value": "somevalue"}],
+            ids=["id"],
+            skip_embedding=True,
+        )
 
+    assert app_with_settings.db.count() == 0
+    app_with_settings.db.reset()
     assert app_with_settings.db.count() == 0
     app_with_settings.db.reset()
 
@@ -205,6 +358,17 @@ def test_chroma_db_collection_collections_are_persistent():
     app.set_collection_name("test_collection_1")
     app.db.collection.add(embeddings=[[0, 0, 0]], ids=["0"])
     del app
+def test_chroma_db_collection_collections_are_persistent():
+    app = App(config=AppConfig(collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db"))
+    app.set_collection_name("test_collection_1")
+    app.db.collection.add(embeddings=[[0, 0, 0]], ids=["0"])
+    del app
+
+    app = App(config=AppConfig(collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db"))
+    app.set_collection_name("test_collection_1")
+    assert app.db.count() == 1
+
+    app.db.reset()
 
     app = App(config=AppConfig(collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db"))
     app.set_collection_name("test_collection_1")
@@ -222,7 +386,19 @@ def test_chroma_db_collection_parallel_collections():
         AppConfig(collection_name="test_collection_2", collect_metrics=False),
         db_config=ChromaDbConfig(allow_reset=True, dir="test-db"),
     )
+def test_chroma_db_collection_parallel_collections():
+    app1 = App(
+        AppConfig(collection_name="test_collection_1", collect_metrics=False),
+        db_config=ChromaDbConfig(allow_reset=True, dir="test-db"),
+    )
+    app2 = App(
+        AppConfig(collection_name="test_collection_2", collect_metrics=False),
+        db_config=ChromaDbConfig(allow_reset=True, dir="test-db"),
+    )
 
+    # cleanup if any previous tests failed or were interrupted
+    app1.db.reset()
+    app2.db.reset()
     # cleanup if any previous tests failed or were interrupted
     app1.db.reset()
     app2.db.reset()
@@ -230,10 +406,23 @@ def test_chroma_db_collection_parallel_collections():
     app1.db.collection.add(embeddings=[0, 0, 0], ids=["0"])
     assert app1.db.count() == 1
     assert app2.db.count() == 0
+    app1.db.collection.add(embeddings=[0, 0, 0], ids=["0"])
+    assert app1.db.count() == 1
+    assert app2.db.count() == 0
 
     app1.db.collection.add(embeddings=[[0, 0, 0], [1, 1, 1]], ids=["1", "2"])
     app2.db.collection.add(embeddings=[0, 0, 0], ids=["0"])
+    app1.db.collection.add(embeddings=[[0, 0, 0], [1, 1, 1]], ids=["1", "2"])
+    app2.db.collection.add(embeddings=[0, 0, 0], ids=["0"])
 
+    app1.set_collection_name("test_collection_2")
+    assert app1.db.count() == 1
+    app2.set_collection_name("test_collection_1")
+    assert app2.db.count() == 3
+
+    # cleanup
+    app1.db.reset()
+    app2.db.reset()
     app1.set_collection_name("test_collection_2")
     assert app1.db.count() == 1
     app2.set_collection_name("test_collection_1")
@@ -253,10 +442,27 @@ def test_chroma_db_collection_ids_share_collections():
         AppConfig(id="new_app_id_2", collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db")
     )
     app2.set_collection_name("one_collection")
+def test_chroma_db_collection_ids_share_collections():
+    app1 = App(
+        AppConfig(id="new_app_id_1", collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db")
+    )
+    app1.set_collection_name("one_collection")
+    app2 = App(
+        AppConfig(id="new_app_id_2", collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db")
+    )
+    app2.set_collection_name("one_collection")
 
     app1.db.collection.add(embeddings=[[0, 0, 0], [1, 1, 1]], ids=["0", "1"])
     app2.db.collection.add(embeddings=[0, 0, 0], ids=["2"])
+    app1.db.collection.add(embeddings=[[0, 0, 0], [1, 1, 1]], ids=["0", "1"])
+    app2.db.collection.add(embeddings=[0, 0, 0], ids=["2"])
 
+    assert app1.db.count() == 3
+    assert app2.db.count() == 3
+
+    # cleanup
+    app1.db.reset()
+    app2.db.reset()
     assert app1.db.count() == 3
     assert app2.db.count() == 3
 
@@ -282,19 +488,49 @@ def test_chroma_db_collection_reset():
         AppConfig(id="new_app_id_4", collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db")
     )
     app4.set_collection_name("four_collection")
+def test_chroma_db_collection_reset():
+    app1 = App(
+        AppConfig(id="new_app_id_1", collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db")
+    )
+    app1.set_collection_name("one_collection")
+    app2 = App(
+        AppConfig(id="new_app_id_2", collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db")
+    )
+    app2.set_collection_name("two_collection")
+    app3 = App(
+        AppConfig(id="new_app_id_1", collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db")
+    )
+    app3.set_collection_name("three_collection")
+    app4 = App(
+        AppConfig(id="new_app_id_4", collect_metrics=False), db_config=ChromaDbConfig(allow_reset=True, dir="test-db")
+    )
+    app4.set_collection_name("four_collection")
 
+    app1.db.collection.add(embeddings=[0, 0, 0], ids=["1"])
+    app2.db.collection.add(embeddings=[0, 0, 0], ids=["2"])
+    app3.db.collection.add(embeddings=[0, 0, 0], ids=["3"])
+    app4.db.collection.add(embeddings=[0, 0, 0], ids=["4"])
     app1.db.collection.add(embeddings=[0, 0, 0], ids=["1"])
     app2.db.collection.add(embeddings=[0, 0, 0], ids=["2"])
     app3.db.collection.add(embeddings=[0, 0, 0], ids=["3"])
     app4.db.collection.add(embeddings=[0, 0, 0], ids=["4"])
 
     app1.db.reset()
+    app1.db.reset()
 
     assert app1.db.count() == 0
     assert app2.db.count() == 1
     assert app3.db.count() == 1
     assert app4.db.count() == 1
+    assert app1.db.count() == 0
+    assert app2.db.count() == 1
+    assert app3.db.count() == 1
+    assert app4.db.count() == 1
 
+    # cleanup
+    app2.db.reset()
+    app3.db.reset()
+    app4.db.reset()
     # cleanup
     app2.db.reset()
     app3.db.reset()
