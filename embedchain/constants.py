5a93e3c8167240f9951896939996d616d78686d4
ghoshaishik505 <47148051+ghoshaishik505@users.noreply.github.com>
2023-11-28 23:26:27 +0530

    Update SQLITE_PATH
    Changed the SQLITE_PATH to be based out of CWD/db for easier development

--- embedchain/constants.py
+++ embedchain/constants.py
@@ -3,6 +3,6 @@ from pathlib import Path
 
 ABS_PATH = os.getcwd()
 HOME_DIR = str(Path.home())
-CONFIG_DIR = os.path.join(HOME_DIR, ".embedchain")
+CONFIG_DIR = os.path.join(ABS_PATH, "db/.embedchain")
 CONFIG_FILE = os.path.join(CONFIG_DIR, "config.json")
 SQLITE_PATH = os.path.join(CONFIG_DIR, "embedchain.db")
