10c9c90f0258d283bb0b503f265ccbc1d5d2624a
sidmohanty11 <sidmohanty11@gmail.com>
2023-12-14 01:41:45 +0530

    dir reader

--- embedchain/utils.py
+++ embedchain/utils.py
@@ -196,8 +196,7 @@ def detect_datatype(source: Any) -> DataType:
     formatted_source = format_source(str(source), 30)
 
     if url:
-        from langchain.document_loaders.youtube import \
-            ALLOWED_NETLOCK as YOUTUBE_ALLOWED_NETLOCS
+        from langchain.document_loaders.youtube import ALLOWED_NETLOCK as YOUTUBE_ALLOWED_NETLOCS
 
         if url.netloc in YOUTUBE_ALLOWED_NETLOCS:
             logging.debug(f"Source of `{formatted_source}` detected as `youtube_video`.")
@@ -226,6 +225,10 @@ def detect_datatype(source: Any) -> DataType:
         if url.path.endswith(".docx"):
             logging.debug(f"Source of `{formatted_source}` detected as `docx`.")
             return DataType.DOCX
+
+        if url.path.endswith(".txt"):
+            logging.debug(f"Source of `{formatted_source}` detected as `text`.")
+            return DataType.TEXT
 
         if url.path.endswith(".yaml"):
             try:
@@ -302,6 +305,10 @@ def detect_datatype(source: Any) -> DataType:
         if source.endswith(".mdx") or source.endswith(".md"):
             logging.debug(f"Source of `{formatted_source}` detected as `mdx`.")
             return DataType.MDX
+
+        if source.endswith(".txt"):
+            logging.debug(f"Source of `{formatted_source}` detected as `text`.")
+            return DataType.TEXT
 
         if source.endswith(".yaml"):
             with open(source, "r") as file:
