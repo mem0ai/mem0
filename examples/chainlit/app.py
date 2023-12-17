a00ce159c9c30846a3efa9bab1b7f73cb6f47bac
sidmohanty11 <sidmohanty11@gmail.com>
2023-12-17 17:07:47 +0530

    chainlit integration

--- examples/chainlit/app.py
+++ examples/chainlit/app.py
@@ -1,21 +1,33 @@
 import chainlit as cl
-from embedchain.llm.openai import OpenAILlm
-from embedchain.config import BaseLlmConfig
+from embedchain import Pipeline as App
 
 import os
 
 os.environ["OPENAI_API_KEY"] = "sk-xxx"
 
-
 @cl.on_chat_start
 async def on_chat_start():
-    config = BaseLlmConfig(stream=True)
-    llm = OpenAILlm(config)
-    cl.user_session.set("llm", llm)
+    app = App.from_config(config={
+        'app': {
+            'config': {
+                'name': 'chainlit-app'
+            }
+        },
+        'llm': {
+            'config': {
+                'stream': True,
+            }
+        }
+    })
+    app.collect_metrics = False
+    cl.user_session.set("app", app)
 
 
 @cl.on_message
 async def on_message(message: cl.Message):
-    llm = cl.user_session.get("llm")
-    response_message = llm.get_llm_model_answer(message.content)
-    await cl.Message(content=response_message).send()
+    app = cl.user_session.get("app")
+    msg = cl.Message(content="")
+    for chunk in await cl.make_async(app.chat)(message.content):
+        await msg.stream_token(chunk)
+    
+    await msg.send()
