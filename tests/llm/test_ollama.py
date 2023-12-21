2dffd4a369a1f458726f5596bd433d369de5fb06
Sukkrit Sharma <sukkritsharma@gmail.com>
2023-12-22 03:48:46 +0530

    Updated Docs and fixed Test Cases

--- tests/llm/test_ollama.py
+++ tests/llm/test_ollama.py
@@ -1,53 +1,43 @@
+import os
+
 import pytest
-from langchain.llms.ollama import Ollama as LangchainOllama
 
 from embedchain.config import BaseLlmConfig
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from embedchain.llm.ollama import OllamaLlm
 
+from langchain.callbacks.manager import CallbackManager
+
 
 @pytest.fixture
-def config():
-    config = BaseLlmConfig(
-        temperature=0.7,
-        top_p=0.8,
-        stream=False,
-        system_prompt="System prompt",
-        model="llama2",
-    )
+def ollama_llm_config():
+    config = BaseLlmConfig(model="llama2", temperature=0.7, top_p=0.8, stream=True, system_prompt=None)
     yield config
 
+def test_get_llm_model_answer(ollama_llm_config, mocker):
+    mocker.patch("embedchain.llm.ollama.OllamaLlm._get_answer", return_value="Test answer")
 
-@pytest.fixture
-def ollama_with_config(config):
-    return OllamaLlm(config=config)
+    llm = OllamaLlm(ollama_llm_config)
+    answer = llm.get_llm_model_answer("Test query")
 
-
-@pytest.fixture
-def ollama_without_config():
-    return OllamaLlm()
+    assert answer == "Test answer"
 
 
-def test_ollama_init_with_config(config, ollama_with_config):
-    assert ollama_with_config.config.temperature == config.temperature
-    assert ollama_with_config.config.top_p == config.top_p
-    assert ollama_with_config.config.stream == config.stream
-    assert ollama_with_config.config.system_prompt == config.system_prompt
-    assert ollama_with_config.config.model == config.model
+def test_get_answer_mocked_ollama(ollama_llm_config, mocker):
+    mocked_ollama = mocker.patch("embedchain.llm.ollama.Ollama")
+    mock_instance = mocked_ollama.return_value
+    mock_instance.return_value = "Mocked answer"
 
-    assert isinstance(ollama_with_config.instance, LangchainOllama)
+    llm = OllamaLlm(ollama_llm_config)
+    prompt = "Test query"
+    answer = llm.get_llm_model_answer(prompt)
 
-
-def test_ollama_init_without_config(ollama_without_config):
-    assert ollama_without_config.config.model == "llama2"
-    assert isinstance(ollama_without_config.instance, LangchainOllama)
-
-
-def test_get_llm_model_answer(mocker, ollama_with_config):
-    test_query = "Test query"
-    test_answer = "Test answer"
-
-    mocked_get_answer = mocker.patch("embedchain.llm.ollama.OllamaLlm._get_answer", return_value=test_answer)
-    answer = ollama_with_config.get_llm_model_answer(test_query)
-
-    assert answer == test_answer
-    mocked_get_answer.assert_called_once_with(prompt=test_query, config=ollama_with_config.config)
+    assert answer == "Mocked answer"
+    mocked_ollama.assert_called_once_with(
+        model="llama2",
+        system=None,
+        temperature=0.7,
+        top_p=0.8,
+        callback_manager=mocker.ANY  # Use mocker.ANY to ignore the exact instance
+    )
+    mock_instance.assert_called_once_with(prompt)
