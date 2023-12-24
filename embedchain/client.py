0b6c982ee21b2f339354920cfb7d16d2a1d0a01a
Deshraj Yadav <deshrajdry@gmail.com>
2023-12-24 15:26:59 +0530

    [Bug Fix] Fix FileExistsError when using Embedchain in multithreading environment

--- embedchain/client.py
+++ embedchain/client.py
@@ -39,9 +39,7 @@ class Client:
         :return: user id
         :rtype: str
         """
-        if not os.path.exists(CONFIG_DIR):
-            os.makedirs(CONFIG_DIR)
-
+        os.makedirs(CONFIG_DIR, exist_ok=True)
         if os.path.exists(CONFIG_FILE):
             with open(CONFIG_FILE, "r") as f:
                 data = json.load(f)
--- embedchain/telemetry/posthog.py
+++ embedchain/telemetry/posthog.py
@@ -39,9 +39,7 @@ class AnonymousTelemetry:
         posthog_logger.disabled = True
 
     def _get_user_id(self):
-        if not os.path.exists(CONFIG_DIR):
-            os.makedirs(CONFIG_DIR)
-
+        os.makedirs(CONFIG_DIR, exist_ok=True)
         if os.path.exists(CONFIG_FILE):
             with open(CONFIG_FILE, "r") as f:
                 data = json.load(f)
