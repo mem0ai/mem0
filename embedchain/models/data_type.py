7c2f84541b2808c9011e74231dad7fbe52dd879d
sidmohanty11 <sidmohanty11@gmail.com>
2023-12-29 20:54:43 +0530

    read local text files of any kind - code, txts, json etc and fix directory

--- embedchain/models/data_type.py
+++ embedchain/models/data_type.py
@@ -37,6 +37,7 @@ class IndirectDataType(Enum):
     BEEHIIV = "beehiiv"
     DIRECTORY = "directory"
     SLACK = "slack"
+    TEXT_FILE = "text_file"
 
 
 class SpecialDataType(Enum):
@@ -73,3 +74,4 @@ class DataType(Enum):
     BEEHIIV = IndirectDataType.BEEHIIV.value
     DIRECTORY = IndirectDataType.DIRECTORY.value
     SLACK = IndirectDataType.SLACK.value
+    TEXT_FILE = IndirectDataType.TEXT_FILE.value
