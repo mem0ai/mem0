a54dde05093649a48fb09227520fb33c8cb0796d
Deshraj Yadav <deshrajdry@gmail.com>
2023-12-30 15:36:24 +0530

    [Bug Fix] Handle chat sessions properly during `app.chat()` calls (#1084)

--- tests/memory/test_memory_messages.py
+++ tests/memory/test_memory_messages.py
@@ -3,17 +3,17 @@ from embedchain.memory.message import BaseMessage, ChatMessage
 
 def test_ec_base_message():
     content = "Hello, how are you?"
-    creator = "human"
+    created_by = "human"
     metadata = {"key": "value"}
 
-    message = BaseMessage(content=content, creator=creator, metadata=metadata)
+    message = BaseMessage(content=content, created_by=created_by, metadata=metadata)
 
     assert message.content == content
-    assert message.creator == creator
+    assert message.created_by == created_by
     assert message.metadata == metadata
     assert message.type is None
     assert message.is_lc_serializable() is True
-    assert str(message) == f"{creator}: {content}"
+    assert str(message) == f"{created_by}: {content}"
 
 
 def test_ec_base_chat_message():
@@ -27,11 +27,11 @@ def test_ec_base_chat_message():
     chat_message.add_ai_message(ai_message_content, metadata=ai_metadata)
 
     assert chat_message.human_message.content == human_message_content
-    assert chat_message.human_message.creator == "human"
+    assert chat_message.human_message.created_by == "human"
     assert chat_message.human_message.metadata == human_metadata
 
     assert chat_message.ai_message.content == ai_message_content
-    assert chat_message.ai_message.creator == "ai"
+    assert chat_message.ai_message.created_by == "ai"
     assert chat_message.ai_message.metadata == ai_metadata
 
     assert str(chat_message) == f"human: {human_message_content}\nai: {ai_message_content}"
