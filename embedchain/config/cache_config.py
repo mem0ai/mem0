2e57fb8f0d3f962c0fbd4ddc06454d885c67c247
Deven Patel <deven298@yahoo.com>
2023-12-31 23:30:53 +0530

    WIP

--- embedchain/config/cache_config.py
+++ embedchain/config/cache_config.py
@@ -8,7 +8,7 @@ from embedchain.helpers.json_serializable import register_deserializable
 class CacheConfig(BaseConfig):
     def __init__(
         self,
-        similarity_threshold: Optional[float] = 0.5,
+        similarity_threshold: Optional[float] = 0.8,
     ):
         if similarity_threshold < 0 or similarity_threshold > 1:
             raise ValueError(f"similarity_threshold {similarity_threshold} should be between 0 and 1")
