c7f5a9322aa02609238cdf002833e804fe4b0f18
Mritunjay Prasad <mritunjayprasad91@gmail.com>
2024-01-04 16:52:47 +0530

    Added lines to incorporate youtube videos not having transcripts, by generating from an api, using authenticator key

--- embedchain/loaders/youtube_video.py
+++ embedchain/loaders/youtube_video.py
@@ -1,4 +1,10 @@
 import hashlib
+import requests
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 try:
     from langchain.document_loaders import YoutubeLoader
@@ -19,10 +25,22 @@ class YoutubeVideoLoader(BaseLoader):
         doc = loader.load()
         output = []
         if not len(doc):
-            raise ValueError(f"No data found for url: {url}")
-        content = doc[0].page_content
+            transcription_url = "https://transcript.lol/api/createTranscript"
+            payload = {
+                "sourceUrl": f"{url}",
+                "transcriptLangCode": "es"
+            }
+            headers = {
+                "X-API-KEY": os.environ['Transcript_API_Key'],
+                "Content-Type": "application/json"
+            }
+            response = requests.request("POST", transcription_url, json=payload, headers=headers)
+            content = response.text
+            meta_data = {}
+        else:
+            content = doc[0].page_content
+            meta_data = doc[0].metadata
         content = clean_string(content)
-        meta_data = doc[0].metadata
         meta_data["url"] = url
 
         output.append(
