c2b5f2c4d3ff5949c3a6725cf9df0e8976fcd086
Deven Patel <deven298@yahoo.com>
2024-01-19 12:48:00 +0530

    add docs

--- embedchain/llm/aws_bedrock.py
+++ embedchain/llm/aws_bedrock.py
@@ -1,6 +1,4 @@
-import os
-import logging
-from typing import Any, Optional
+from typing import Optional
 
 from langchain.chat_models import BedrockChat
 from langchain.schema import HumanMessage, SystemMessage
@@ -14,12 +12,11 @@ from embedchain.llm.base import BaseLlm
 class AWSBedrockLlm(BaseLlm):
     def __init__(self, config: Optional[BaseLlmConfig] = None):
         super().__init__(config)
-    
+
     def get_llm_model_answer(self, prompt) -> str:
         response = self._get_answer(prompt, self.config)
         return response
-    
-    
+
     def _get_answer(self, prompt: str, config: BaseLlmConfig) -> str:
         try:
             import boto3
@@ -28,25 +25,24 @@ class AWSBedrockLlm(BaseLlm):
                 "The required dependencies for AWSBedrock are not installed."
                 'Please install with `pip install --upgrade "embedchain[aws-bedrock]"`'
             ) from None
-        
-        self.boto_client = boto3.client('bedrock-runtime', 'us-west-2')
-        
+
+        self.boto_client = boto3.client("bedrock-runtime", "us-west-2")
+
         messages = []
         if config.system_prompt:
             messages.append(SystemMessage(content=config.system_prompt))
         messages.append(HumanMessage(content=prompt))
         kwargs = {
-            "model_id": config.model or "anthropic.claude-v2",
+            "model_id": config.model or "meta.llama2-13b-chat-v1",
             "client": self.boto_client,
             "model_kwargs": {
                 "temperature": config.temperature,
-                "max_tokens_to_sample": config.max_tokens,
+                "max_tokens": config.max_tokens,
             },
         }
         if config.top_p:
             kwargs["model_kwargs"]["top_p"] = config.top_p
 
-        import pdb; pdb.set_trace()
         if config.stream:
             from langchain.callbacks.streaming_stdout import \
                 StreamingStdOutCallbackHandler
@@ -55,5 +51,5 @@ class AWSBedrockLlm(BaseLlm):
             chat = BedrockChat(**kwargs, streaming=config.stream, callbacks=callbacks)
         else:
             chat = BedrockChat(**kwargs)
-        
+
         return chat(messages).content
