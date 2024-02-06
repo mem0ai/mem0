0e3b0e06402eed7cb39e21f9b34cca566b170faf
Deshraj Yadav <deshrajdry@gmail.com>
2024-02-06 15:26:22 -0800

    [Feature] Add support for metadata filtering on search API

--- embedchain/config/vectordb/pinecone.py
+++ embedchain/config/vectordb/pinecone.py
@@ -9,10 +9,8 @@ from embedchain.helpers.json_serializable import register_deserializable
 class PineconeDBConfig(BaseVectorDbConfig):
     def __init__(
         self,
-        collection_name: Optional[str] = None,
-        api_key: Optional[str] = None,
         index_name: Optional[str] = None,
-        dir: Optional[str] = None,
+        api_key: Optional[str] = None,
         vector_dimension: int = 1536,
         metric: Optional[str] = "cosine",
         pod_config: Optional[dict[str, any]] = None,
@@ -21,9 +19,9 @@ class PineconeDBConfig(BaseVectorDbConfig):
     ):
         self.metric = metric
         self.api_key = api_key
+        self.index_name = index_name
         self.vector_dimension = vector_dimension
         self.extra_params = extra_params
-        self.index_name = index_name or f"{collection_name}-{vector_dimension}".lower().replace("_", "-")
         if pod_config is None and serverless_config is None:
             # If no config is provided, use the default pod spec config
             pod_environment = os.environ.get("PINECONE_ENV", "gcp-starter")
@@ -35,4 +33,4 @@ class PineconeDBConfig(BaseVectorDbConfig):
         if self.pod_config and self.serverless_config:
             raise ValueError("Only one of pod_config or serverless_config can be provided.")
 
-        super().__init__(collection_name=collection_name, dir=None)
+        super().__init__(collection_name=self.index_name, dir=None)
