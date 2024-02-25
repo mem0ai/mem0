2b6a75dc8a88caea89ce7f2033adfa83834f893b
Deshraj Yadav <deshrajdry@gmail.com>
2024-02-25 11:45:14 -0800

    [Feature] Add support for Groq LLMs

--- embedchain/llm/openai.py
+++ embedchain/llm/openai.py
@@ -58,8 +58,7 @@ class OpenAILlm(BaseLlm):
         messages: list[BaseMessage],
     ) -> str:
         from langchain.output_parsers.openai_tools import JsonOutputToolsParser
-        from langchain_core.utils.function_calling import \
-            convert_to_openai_tool
+        from langchain_core.utils.function_calling import convert_to_openai_tool
 
         openai_tools = [convert_to_openai_tool(tools)]
         chat = chat.bind(tools=openai_tools).pipe(JsonOutputToolsParser())
