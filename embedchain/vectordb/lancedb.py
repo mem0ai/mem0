55413b9a9ccd28f5ecf2b0e6fee23b0cd16af3cd
PrashantDixit-dev <prasantdixit987@gmail.com>
2024-03-15 22:44:43 +0530

    tested

---
+++ embedchain/vectordb/lancedb.py
@@ -0,0 +1,300 @@
+from typing import Any, Dict, List, Optional, Union
+
+import pandas as pd
+import pyarrow as pa
+
+try:
+    import lancedb
+except ImportError:
+    raise ImportError("LanceDB is required. Install with `pip install lancedb`") from None
+
+from embedchain.config.vectordb.lancedb import LanceDBConfig
+from embedchain.helpers.json_serializable import register_deserializable
+from embedchain.vectordb.base import BaseVectorDB
+
+
+@register_deserializable
+class LanceDB(BaseVectorDB):
+    """
+    LanceDB as vector database
+    """
+
+    BATCH_SIZE = 100
+
+    def __init__(
+        self,
+        config: Optional[LanceDBConfig] = None,
+    ):
+        """LanceDB as vector database.
+
+        :param config: LanceDB database config, defaults to None
+        :type config: LanceDBConfig, optional
+        """
+        if config:
+            self.config = config
+        else:
+            self.config = LanceDBConfig()
+
+        if self.config.dir is None:
+            self.client = lancedb.connect("~/.lancedb")
+        else:
+            self.client = lancedb.connect(self.config.dir)
+
+        super().__init__(config=self.config)
+
+    def _initialize(self):
+        """
+        This method is needed because `embedder` attribute needs to be set externally before it can be initialized.
+        """
+        self._get_or_create_collection(self.config.collection_name)
+
+    def _get_or_create_db(self):
+        """
+        Called during initialization
+        """
+        return self.client
+
+    @staticmethod
+    def _generate_where_clause(self, where: Dict[str, any]) -> str:
+        """
+        This method
+        """
+
+        where_filters = ""
+
+        if len(where.keys()) == 1:
+            where_filters = f"{where.keys()[0]} = {where.values()[0]}"
+            return where_filters
+
+        where_items = list(where.items())
+        where_count = len(where_items)
+
+        for i, (key, value) in enumerate(where_items, start=1):
+            condition = f"{key} = {value} AND "
+            where_filters += condition
+
+            if i == where_count:
+                condition = f"{key} = {value}"
+                where_filters += condition
+
+        return where_filters
+
+    def _get_or_create_collection(self, table_name: str, reset=False):
+        """
+        Get or create a named collection.
+
+        :param name: Name of the collection
+        :type name: str
+        :return: Created collection
+        :rtype: Collection
+        """
+        if not self.embedder:
+            raise ValueError("Embedder not set. Please set an embedder with `set_embedder` before initialization.")
+
+        schema = pa.schema(
+            [
+                pa.field("vector", pa.list_(pa.float32(), list_size=self.embedder.vector_dimension)),
+                pa.field("doc", pa.string()),
+                pa.field("metadata", pa.string()),
+                pa.field("id", pa.string()),
+            ]
+        )
+
+        if not reset:
+            if table_name not in self.client.table_names():
+                self.collection = self.client.create_table(table_name, schema=schema)
+
+            self.collection = self.client[table_name]
+
+        else:
+            self.client.drop_table(table_name)
+            self.collection = self.client.create_table(table_name, schema=schema)
+            self.collection = self.client[table_name]
+
+        return self.collection
+
+    def get(self, ids: Optional[List[str]] = None, where: Optional[Dict[str, any]] = None, limit: Optional[int] = None):
+        """
+        Get existing doc ids present in vector database
+
+        :param ids: list of doc ids to check for existence
+        :type ids: List[str]
+        :param where: Optional. to filter data
+        :type where: Dict[str, Any]
+        :param limit: Optional. maximum number of documents
+        :type limit: Optional[int]
+        :return: Existing documents.
+        :rtype: List[str]
+        """
+        if limit is not None:
+            max_limit = limit
+        else:
+            max_limit = 5
+        results = {"ids": [], "metadatas": []}
+        if ids is not None:
+            if where is not None:
+                where_clause = self._generate_where_clause(where)
+                for id in ids:
+                    result = self.collection.search(id).where(where_clause).limit(max_limit).to_list()
+                    results["ids"].append([r["id"] for r in result])
+                    results["metadatas"].append([r["metadata"] for r in result])
+            else:
+                for id in ids:
+                    result = self.collection.search(id).limit(max_limit).to_list()
+                    results["ids"].append([r["id"] for r in result])
+                    results["metadatas"].append([r["metadata"] for r in result])
+        return results
+
+    def add(
+        self,
+        embeddings: List[List[float]],
+        documents: List[str],
+        metadatas: List[object],
+        ids: List[str],
+        skip_embedding: bool,
+    ) -> Any:
+        """
+        Add vectors to lancedb database
+
+        :param embeddings: list of embeddings to add
+        :type embeddings: List[List[float]]
+        :param documents: Documents
+        :type documents: List[str]
+        :param metadatas: Metadatas
+        :type metadatas: List[object]
+        :param ids: ids
+        :type ids: List[str]
+        :param skip_embedding: Optional. If True, then the embeddings are assumed to be already generated.
+        :type skip_embedding: bool
+        """
+        if skip_embedding:
+            data = []
+            to_ingest = list(zip(embeddings, documents, metadatas, ids))
+            for emb, doc, meta, id in to_ingest:
+                temp = {}
+                temp["vector"] = emb
+                temp["doc"] = doc
+                temp["metadata"] = str(meta)
+                temp["id"] = id
+                data.append(temp)
+        else:
+            data = []
+            to_ingest = list(zip(documents, metadatas, ids))
+            for doc, meta, id in to_ingest:
+                temp = {}
+                temp["doc"] = doc
+                temp["metadata"] = str(meta)
+                temp["id"] = id
+                data.append(temp)
+
+        df = pd.DataFrame(data)
+        self.collection.add(df)
+
+    def _format_result(self, results) -> list:
+        """
+        Format LanceDB results
+
+        :param results: LanceDB query results to format.
+        :type results: QueryResult
+        :return: Formatted results
+        :rtype: list[tuple[Document, float]]
+        """
+        return results.to_list()
+
+    def query(
+        self,
+        input_query: list[str],
+        n_results: int,
+        where: Optional[dict[str, any]] = None,
+        raw_filter: Optional[dict[str, any]] = None,
+        citations: bool = False,
+        **kwargs: Optional[dict[str, any]],
+    ) -> Union[list[tuple[str, dict]], list[str]]:
+        """
+        Query contents from vector database based on vector similarity
+
+        :param input_query: list of query string
+        :type input_query: list[str]
+        :param n_results: no of similar documents to fetch from database
+        :type n_results: int
+        :param where: to filter data
+        :type where: dict[str, Any]
+        :param raw_filter: Raw filter to apply
+        :type raw_filter: dict[str, Any]
+        :param citations: we use citations boolean param to return context along with the answer.
+        :type citations: bool, default is False.
+        :raises InvalidDimensionException: Dimensions do not match.
+        :return: The content of the document that matched your query,
+        along with url of the source and doc_id (if citations flag is true)
+        :rtype: list[str], if citations=False, otherwise list[tuple[str, str, str]]
+        """
+        if where and raw_filter:
+            raise ValueError("Both `where` and `raw_filter` cannot be used together.")
+
+        where_clause = {}
+        if raw_filter:
+            where_clause = raw_filter
+        if where:
+            where_clause = self._generate_where_clause(where)
+        try:
+            result = self.collection.search(query_embeddings=input_query).where(where_clause).limit(n_results)
+
+        except Exception as e:
+            e.message()
+            +". This is commonly a side-effect when an embedding function, different from the one used to add the embeddings, is used to retrieve an embedding from the database."  # noqa E501
+
+        results_formatted = self._format_result(result)
+
+        contexts = []
+        for result in results_formatted:
+            context = result[0]
+            if citations:
+                metadata = context["metadata"]
+                contexts.append((context, metadata))
+            else:
+                contexts.append(context["doc"])
+        return contexts
+
+    def set_collection_name(self, name: str):
+        """
+        Set the name of the collection. A collection is an isolated space for vectors.
+
+        :param name: Name of the collection.
+        :type name: str
+        """
+        if not isinstance(name, str):
+            raise TypeError("Collection name must be a string")
+        self.config.collection_name = name
+        self._get_or_create_collection(self.config.collection_name)
+
+    def count(self) -> int:
+        """
+        Count number of documents/chunks embedded in the database.
+
+        :return: number of documents
+        :rtype: int
+        """
+        return self.collection.count_rows()
+
+    def delete(self, where):
+        return self.collection.delete(where=where)
+
+    def reset(self):
+        """
+        Resets the database. Deletes all embeddings irreversibly.
+        """
+        # Delete all data from the collection and recreate collection
+        if self.config.allow_reset:
+            try:
+                self._get_or_create_collection(self.config.collection_name, reset=True)
+            except ValueError:
+                raise ValueError(
+                    "For safety reasons, resetting is disabled. "
+                    "Please enable it by setting `allow_reset=True` in your LanceDbConfig"
+                ) from None
+        # Recreate
+        else:
+            print(
+                "For safety reasons, resetting is disabled. "
+                "Please enable it by setting `allow_reset=True` in your LanceDbConfig"
+            )
---
+++ tests/vectordb/test_lancedb.py
@@ -0,0 +1,236 @@
+import os
+import shutil
+
+import pytest
+
+from embedchain import App
+from embedchain.config import AppConfig, LanceDBConfig
+from embedchain.vectordb.lancedb import LanceDB
+
+os.environ["OPENAI_API_KEY"] = "test-api-key"
+
+
+@pytest.fixture
+def lancedb():
+    return LanceDB(config=LanceDBConfig(dir="test-db", collection_name="test-coll"))
+
+
+@pytest.fixture
+def app_with_settings():
+    lancedb_config = LanceDBConfig(allow_reset=True, dir="test-db-reset")
+    lancedb = LanceDB(config=lancedb_config)
+    app_config = AppConfig(collect_metrics=False)
+    return App(config=app_config, db=lancedb)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def cleanup_db():
+    yield
+    try:
+        shutil.rmtree("test-db.lance")
+    except OSError as e:
+        print("Error: %s - %s." % (e.filename, e.strerror))
+
+
+@pytest.mark.skip(reason="Logging setup needs to be fixed to make this test to work")
+def test_lancedb_duplicates_throw_warning(caplog):
+    db = LanceDB(config=LanceDBConfig(allow_reset=True, dir="test-db"))
+    app = App(config=AppConfig(collect_metrics=False), db=db)
+    embedding = generate_embeddings(0, 1536)
+    app.db.collection.add(embeddings=[embedding], ids=["0"])
+    app.db.collection.add(embeddings=[embedding], ids=["0"])
+    assert "Insert of existing embedding ID: 0" in caplog.text
+    assert "Add of existing embedding ID: 0" in caplog.text
+    app.db.reset()
+
+
+def test_lancedb_duplicates_collections_no_warning(caplog):
+    db = LanceDB(config=LanceDBConfig(allow_reset=True, dir="test-db"))
+    app = App(config=AppConfig(collect_metrics=False), db=db)
+    embedding = generate_embeddings(0, 1536)
+    app.set_collection_name("test_collection_1")
+    app.db.add(embeddings=[embedding], ids=["0"], documents=["doc1"], metadatas=["test"], skip_embedding=True)
+    app.set_collection_name("test_collection_2")
+    app.db.add(embeddings=[embedding], ids=["0"], documents=["doc1"], metadatas=["test"], skip_embedding=True)
+    assert "Insert of existing embedding ID: 0" not in caplog.text
+    assert "Add of existing embedding ID: 0" not in caplog.text
+    app.db.reset()
+    app.set_collection_name("test_collection_1")
+    app.db.reset()
+
+
+def test_lancedb_collection_init_with_default_collection():
+    db = LanceDB(config=LanceDBConfig(allow_reset=True, dir="test-db"))
+    app = App(config=AppConfig(collect_metrics=False), db=db)
+    assert app.db.collection.name == "embedchain_store"
+
+
+def test_lancedb_collection_init_with_custom_collection():
+    db = LanceDB(config=LanceDBConfig(allow_reset=True, dir="test-db"))
+    app = App(config=AppConfig(collect_metrics=False), db=db)
+    app.set_collection_name(name="test_collection")
+    assert app.db.collection.name == "test_collection"
+
+
+def test_lancedb_collection_set_collection_name():
+    db = LanceDB(config=LanceDBConfig(allow_reset=True, dir="test-db"))
+    app = App(config=AppConfig(collect_metrics=False), db=db)
+    app.set_collection_name("test_collection")
+    assert app.db.collection.name == "test_collection"
+
+
+def test_lancedb_collection_changes_encapsulated():
+    db = LanceDB(config=LanceDBConfig(allow_reset=True, dir="test-db"))
+    app = App(config=AppConfig(collect_metrics=False), db=db)
+    app.set_collection_name("test_collection_1")
+    assert app.db.count() == 0
+    embedding = generate_embeddings(0, 1536)
+    app.db.add(embeddings=[embedding], ids=["0"], documents=["doc1"], metadatas=["test"], skip_embedding=True)
+    assert app.db.count() == 1
+
+    app.set_collection_name("test_collection_2")
+    assert app.db.count() == 0
+
+    app.db.add(embeddings=[embedding], ids=["0"], documents=["doc1"], metadatas=["test"], skip_embedding=True)
+    app.set_collection_name("test_collection_1")
+    assert app.db.count() == 1
+    app.db.reset()
+    app.set_collection_name("test_collection_2")
+    app.db.reset()
+
+
+def test_lancedb_collection_collections_are_persistent():
+    db = LanceDB(config=LanceDBConfig(allow_reset=True, dir="test-db"))
+    app = App(config=AppConfig(collect_metrics=False), db=db)
+    app.set_collection_name("test_collection_1")
+    embedding = generate_embeddings(0, 1536)
+    app.db.add(embeddings=[embedding], ids=["0"], documents=["doc1"], metadatas=["test"], skip_embedding=True)
+    del app
+
+    db = LanceDB(config=LanceDBConfig(allow_reset=True, dir="test-db"))
+    app = App(config=AppConfig(collect_metrics=False), db=db)
+    app.set_collection_name("test_collection_1")
+    assert app.db.count() == 1
+
+    app.db.reset()
+
+
+def test_lancedb_collection_parallel_collections():
+    db1 = LanceDB(config=LanceDBConfig(allow_reset=True, dir="test-db", collection_name="test_collection_1"))
+    app1 = App(
+        config=AppConfig(collect_metrics=False),
+        db=db1,
+    )
+    db2 = LanceDB(config=LanceDBConfig(allow_reset=True, dir="test-db", collection_name="test_collection_2"))
+    app2 = App(
+        config=AppConfig(collect_metrics=False),
+        db=db2,
+    )
+
+    # cleanup if any previous tests failed or were interrupted
+    app1.db.reset()
+    app2.db.reset()
+
+    embedding = generate_embeddings(0, 1536)
+    embedding1 = generate_embeddings(1, 1536)
+    app1.db.add(embeddings=[embedding], ids=["0"], documents=["doc1"], metadatas=["test"], skip_embedding=True)
+
+    assert app1.db.count() == 1
+    assert app2.db.count() == 0
+
+    app1.db.add(
+        embeddings=[embedding, embedding1],
+        ids=["1", "2"],
+        documents=["doc1", "doc2"],
+        metadatas=["test", "test"],
+        skip_embedding=True,
+    )
+    app2.db.add(embeddings=[embedding], ids=["0"], documents=["doc1"], metadatas=["test"], skip_embedding=True)
+
+    app1.set_collection_name("test_collection_2")
+    assert app1.db.count() == 1
+    app2.set_collection_name("test_collection_1")
+    assert app2.db.count() == 3
+
+    # cleanup
+    app1.db.reset()
+    app2.db.reset()
+
+
+def test_lancedb_collection_ids_share_collections():
+    db1 = LanceDB(config=LanceDBConfig(allow_reset=True, dir="test-db"))
+    app1 = App(config=AppConfig(collect_metrics=False), db=db1)
+    app1.set_collection_name("one_collection")
+    db2 = LanceDB(config=LanceDBConfig(allow_reset=True, dir="test-db"))
+    app2 = App(config=AppConfig(collect_metrics=False), db=db2)
+    app2.set_collection_name("one_collection")
+
+    # cleanup
+    app1.db.reset()
+    app2.db.reset()
+
+    embedding = generate_embeddings(0, 1536)
+    embedding1 = generate_embeddings(1, 1536)
+    embedding2 = generate_embeddings(2, 1536)
+    app1.db.add(
+        embeddings=[embedding, embedding1],
+        ids=["0", "1"],
+        documents=["doc1", "doc2"],
+        metadatas=["test", "test"],
+        skip_embedding=True,
+    )
+    app2.db.add(embeddings=[embedding2], ids=["2"], documents=["doc3"], metadatas=["test"], skip_embedding=True)
+
+    assert app1.db.count() == 2
+    assert app2.db.count() == 3
+
+    # cleanup
+    app1.db.reset()
+    app2.db.reset()
+
+
+def test_lancedb_collection_reset():
+    db1 = LanceDB(config=LanceDBConfig(allow_reset=True, dir="test-db"))
+    app1 = App(config=AppConfig(collect_metrics=False), db=db1)
+    app1.set_collection_name("one_collection")
+    db2 = LanceDB(config=LanceDBConfig(allow_reset=True, dir="test-db"))
+    app2 = App(config=AppConfig(collect_metrics=False), db=db2)
+    app2.set_collection_name("two_collection")
+    db3 = LanceDB(config=LanceDBConfig(allow_reset=True, dir="test-db"))
+    app3 = App(config=AppConfig(collect_metrics=False), db=db3)
+    app3.set_collection_name("three_collection")
+    db4 = LanceDB(config=LanceDBConfig(allow_reset=True, dir="test-db"))
+    app4 = App(config=AppConfig(collect_metrics=False), db=db4)
+    app4.set_collection_name("four_collection")
+
+    # cleanup if any previous tests failed or were interrupted
+    app1.db.reset()
+    app2.db.reset()
+    app3.db.reset()
+    app4.db.reset()
+
+    embedding = generate_embeddings(0, 1536)
+    app1.db.add(embeddings=[embedding], ids=["1"], documents=["doc1"], metadatas=["test"], skip_embedding=True)
+    app2.db.add(embeddings=[embedding], ids=["2"], documents=["doc2"], metadatas=["test"], skip_embedding=True)
+    app3.db.add(embeddings=[embedding], ids=["3"], documents=["doc3"], metadatas=["test"], skip_embedding=True)
+    app4.db.add(embeddings=[embedding], ids=["4"], documents=["doc4"], metadatas=["test"], skip_embedding=True)
+
+    app1.db.reset()
+
+    assert app1.db.count() == 0
+    assert app2.db.count() == 1
+    assert app3.db.count() == 1
+    assert app4.db.count() == 1
+
+    # cleanup
+    app2.db.reset()
+    app3.db.reset()
+    app4.db.reset()
+
+
+def generate_embeddings(dummy_embed, embed_size):
+    generated_embedding = []
+    for i in range(embed_size):
+        generated_embedding.append(dummy_embed)
+
+    return generated_embedding
