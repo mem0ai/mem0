51ab797bdd0bfa579a69a08bab11de5e3298b41b
mogith-pn <143642606+mogith-pn@users.noreply.github.com>
2024-03-19 16:45:13 +0000

    Clarifai: Modified test_clarifai.py

--- tests/llm/test_clarifai.py
+++ tests/llm/test_clarifai.py
@@ -7,32 +7,18 @@ from embedchain.llm.clarifai import ClarifaiLlm
 
 @pytest.fixture
 def clarifai_llm_config(monkeypatch):
+    monkeypatch.setenv("CLARIFAI_PAT","test_api_key")
     config = BaseLlmConfig(
         model="https://clarifai.com/openai/chat-completion/models/GPT-4",
         model_kwargs={"temperature": 0.7, "max_tokens": 100},
     )
     yield config
+    monkeypatch.delenv("CLARIFAI_PAT")
 
+def test_clarifai__llm_get_llm_model_answer(clarifai_llm_config, mocker):
+    mocked_get_answer = mocker.patch("embedchain.llm.clarifai.ClarifaiLlm._get_answer", return_value="Test answer")
+    llm = ClarifaiLlm(clarifai_llm_config)
+    answer = llm.get_llm_model_answer("Test query")
 
-def test_clarifai_llm_init_missing_api_key(monkeypatch):
-    monkeypatch.delenv("CLARIFAI_PAT", raising=False)
-    with pytest.raises(ValueError, match="Please set the CLARIFAI_PAT environment variable."):
-        ClarifaiLlm()
-
-
-def test_clarifai_llm_init(monkeypatch):
-    monkeypatch.setenv("CLARIFAI_PAT", "fake_api_key")
-    clarifai_llm = ClarifaiLlm()
-    assert clarifai_llm is not None
-
-
-def test_clarifai__llm_get_llm_model_answer(monkeypatch, clarifai_llm_config):
-    def mock_get_answer(prompt, config):
-        return "Generated Text"
-
-    monkeypatch.setenv("CLARIFAI_PAT", "fake_api_key")
-    monkeypatch.setattr(ClarifaiLlm, "_get_answer", mock_get_answer)
-    clarifai_llm = ClarifaiLlm(config=clarifai_llm_config)
-    result = clarifai_llm.get_llm_model_answer("test prompt")
-
-    assert result == "Generated Text"
+    assert answer == "Test answer"
+    mocked_get_answer.assert_called_once_with("Test query", clarifai_llm_config)
