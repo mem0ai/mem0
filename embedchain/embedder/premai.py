fe3d5d3cc6cebde70aa67f42ff9ada6b557ff0ad
Anindyadeep <proanindyadeep@gmail.com>
2024-05-16 17:11:59 +0530

    Added premai sdk for accessing embdding models

--- embedchain/embedder/premai.py
+++ embedchain/embedder/premai.py
@@ -1,16 +1,25 @@
-import os 
 import importlib
-from typing import Optional 
+import os
+from typing import Optional
+
+from langchain_community.embeddings.premai import PremAIEmbeddings
 
 from embedchain.config import BaseEmbedderConfig
 from embedchain.embedder.base import BaseEmbedder
 from embedchain.models import VectorDimensions
 
-from langchain_community.embeddings.premai import PremAIEmbeddings
+
+class PremAIConfig(BaseEmbedderConfig):
+    def __init__(self, project_id: int, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.project_id = project_id
+
+
+# TODO: create a similar function like MistralAI Embedding function
+
 
 class PremAIEmbedder(BaseEmbedder):
-    def __init__(self, project_id: int, config: Optional[BaseEmbedderConfig] = None) -> None:
-        self.project_id = project_id
+    def __init__(self, config: Optional[PremAIConfig] = None) -> None:
         super().__init__(config=config)
 
         try:
@@ -20,17 +29,15 @@ class PremAIEmbedder(BaseEmbedder):
                 "The required dependencies for PremAI are not installed."
                 'Please install with `pip install --upgrade "embedchain[premai]"`'
             ) from None
-        
-        _api_key = config.api_key or os.getenv("PREMAI_API_KEY")
+
+        _api_key = self.config.api_key or os.getenv("PREMAI_API_KEY")
         if _api_key is None:
-            raise ValueError(
-                "Please set PREMAI_API_KEY environment variable or pass in the config."
-            )
-        
-        embeddings = PremAIEmbeddings(project_id=project_id, premai_api_key=_api_key)
+            raise ValueError("Please set PREMAI_API_KEY environment variable or pass in the config.")
+        os.environ["PREMAI_API_KEY"] = _api_key
+
+        embeddings = PremAIEmbeddings(project_id=config.project_id, model=config.model)
         embedding_fn = BaseEmbedder._langchain_default_concept(embeddings)
         self.set_embedding_fn(embedding_fn=embedding_fn)
 
         vector_dimensions = self.config.vector_dimension or VectorDimensions.OPENAI
         self.set_vector_dimension(vector_dimension=vector_dimensions)
-        
