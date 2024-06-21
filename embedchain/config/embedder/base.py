08c9e2d339ede7aa0bee61827bcc81b5df11c959
Dev Khant <devkhant24@gmail.com>
2024-06-21 21:27:21 +0530

    Add HF endpoint in embedder (#1436)

--- embedchain/config/embedder/base.py
+++ embedchain/config/embedder/base.py
@@ -10,6 +10,7 @@ class BaseEmbedderConfig:
         model: Optional[str] = None,
         deployment_name: Optional[str] = None,
         vector_dimension: Optional[int] = None,
+        endpoint: Optional[str] = None,
         api_key: Optional[str] = None,
         api_base: Optional[str] = None,
     ):
@@ -20,9 +21,18 @@ class BaseEmbedderConfig:
         :type model: Optional[str], optional
         :param deployment_name: deployment name for llm embedding model, defaults to None
         :type deployment_name: Optional[str], optional
+        :param vector_dimension: vector dimension of the embedding model, defaults to None
+        :type vector_dimension: Optional[int], optional
+        :param endpoint: endpoint for the embedding model, defaults to None
+        :type endpoint: Optional[str], optional
+        :param api_key: hugginface api key, defaults to None
+        :type api_key: Optional[str], optional
+        :param api_base: huggingface api base, defaults to None
+        :type api_base: Optional[str], optional
         """
         self.model = model
         self.deployment_name = deployment_name
         self.vector_dimension = vector_dimension
+        self.endpoint = endpoint
         self.api_key = api_key
         self.api_base = api_base
