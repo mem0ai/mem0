70031312e9318f9c6aeb09404428523f0808610c
Vatsal Rathod <vatsalrathod16@gmail.com>
2024-06-22 13:54:55 -0400

    Seperated OpenAI and AzureAi embedder.

--- embedchain/embedder/azureopenai.py
+++ embedchain/embedder/azureopenai.py
@@ -9,7 +9,7 @@ from embedchain.embedder.base import BaseEmbedder
 from embedchain.models import VectorDimensions
 
 
-class OpenAIEmbedder(BaseEmbedder):
+class AzureOpenAIEmbedder(BaseEmbedder):
     def __init__(self, config: Optional[BaseEmbedderConfig] = None):
         super().__init__(config=config)
 
@@ -19,17 +19,9 @@ class OpenAIEmbedder(BaseEmbedder):
         api_key = self.config.api_key or os.environ["OPENAI_API_KEY"]
         api_base = self.config.api_base or os.environ.get("OPENAI_API_BASE")
 
+        embeddings = AzureOpenAIEmbeddings(deployment=self.config.deployment_name)
+        embedding_fn = BaseEmbedder._langchain_default_concept(embeddings)
 
-        if api_key is None and os.getenv("OPENAI_ORGANIZATION") is None:
-            raise ValueError(
-                "OPENAI_API_KEY or OPENAI_ORGANIZATION environment variables not provided"
-            )  # noqa:E501
-        embedding_fn = OpenAIEmbeddingFunction(
-            api_key=api_key,
-            api_base=api_base,
-            organization_id=os.getenv("OPENAI_ORGANIZATION"),
-            model_name=self.config.model,
-        )
         self.set_embedding_fn(embedding_fn=embedding_fn)
         vector_dimension = self.config.vector_dimension or VectorDimensions.OPENAI.value
         self.set_vector_dimension(vector_dimension=vector_dimension)
