c4fb777a671dd93e28702d639557d9a4c4d50527
sumant tirkey <sumant@sumants-Laptop.local>
2024-07-09 23:29:00 +0530

    changed imports from community to direct api

--- embedchain/embedder/openai.py
+++ embedchain/embedder/openai.py
@@ -1,7 +1,7 @@
 import os
 from typing import Optional
 
-from langchain_community.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 
 
 from embedchain.config import BaseEmbedderConfig
