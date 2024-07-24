95635fa8ade1b5ae7a677923ef1ea9823d801c3a
Mitul Kataria <53906546+kmitul@users.noreply.github.com>
2024-07-24 16:58:04 +0900

    Update huggingface.py
    Added `dims` attribute to `HuggingFaceEmbedding` using `get_sentence_embedding_dimension` method of `SentenceTransformer`

--- mem0/embeddings/huggingface.py
+++ mem0/embeddings/huggingface.py
@@ -5,6 +5,7 @@ from sentence_transformers import SentenceTransformer
 class HuggingFaceEmbedding(EmbeddingBase):
     def __init__(self, model_name="multi-qa-MiniLM-L6-cos-v1"):
         self.model = SentenceTransformer(model_name)
+        self.dims = self.model.get_sentence_embedding_dimension()
 
     def embed(self, text):
         """
