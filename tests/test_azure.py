e303ce427dd206208676c1cd5c42f6cc85b6533c
crazyxixi <55305719+crazyxixi@users.noreply.github.com>
2024-07-29 22:23:30 +0800

    Update test_azure.py

--- tests/test_azure.py
+++ tests/test_azure.py
@@ -4,13 +4,13 @@ import os, time,sys
 sys.path.append("../")
 from mem0 import Memory
 # We tested the scenario where embedding and LLM are provided by different interfaces from azure_openai. If an interface can provide both embedding and LLM simultaneously, then it is sufficient to set the three environment variables: “AZURE_OPENAI_API_KEY”, “AZURE_OPENAI_ENDPOINT”, and “OPENAI_API_VERSION”.
-os.environ["AZURE_OPENAI_API_KEY"] = "702acd8694634c4abce4223479d0bcf8"
-os.environ["AZURE_OPENAI_ENDPOINT"] = "https://yuaiweiwu-gpt4o.openai.azure.com/"
-os.environ["OPENAI_API_VERSION"] = "2024-05-01-preview"
+os.environ["AZURE_OPENAI_API_KEY"] = ""
+os.environ["AZURE_OPENAI_ENDPOINT"] = ""
+os.environ["OPENAI_API_VERSION"] = ""
 
-os.environ["EMBED_AZURE_OPENAI_API_KEY"] = "c70302f0120b4a99b54886a3b1e12610"
-os.environ["EMBED_AZURE_OPENAI_ENDPOINT"] = "https://aitogether-japan.openai.azure.com/"
-os.environ["EMBED_OPENAI_API_VERSION"] = "2023-08-01-preview"
+os.environ["EMBED_AZURE_OPENAI_API_KEY"] = ""
+os.environ["EMBED_AZURE_OPENAI_ENDPOINT"] = ""
+os.environ["EMBED_OPENAI_API_VERSION"] = ""
 
 config = {
     "llm": {
