64be9696d9dc1da482f358a4779c1f0a2e986ba8
Mathew Shen <datahonor@gmail.com>
2024-08-02 04:01:19 +0800

    fix: method name should be same with abstrct base class (#1592)

--- mem0/embeddings/huggingface.py
+++ mem0/embeddings/huggingface.py
@@ -6,7 +6,7 @@ class HuggingFaceEmbedding(EmbeddingBase):
     def __init__(self, model_name="multi-qa-MiniLM-L6-cos-v1"):
         self.model = SentenceTransformer(model_name)
 
-    def get_embedding(self, text):
+    def embed(self, text):
         """
         Get the embedding for the given text using Hugging Face.
 
