e448fcc929c61f205a96bd66d353192628764795
rajib76 <rajib76.gcp@gmail.com>
2024-08-09 09:48:20 -0700

    changed qdrant

--- cookbooks/add_memory_using_qdrant_cloud.py
+++ cookbooks/add_memory_using_qdrant_cloud.py
@@ -3,15 +3,14 @@ import os
 from dotenv import load_dotenv
 from mem0 import Memory
 
-# Loading my API_KEY for OPENAI
+# Loading OpenAI API Key
 load_dotenv()
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
 USER_ID = "test"
-os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
-quadrant_host="xx.gcp.cloud.qdrant.io"
+quadrant_host="668a5b8f-6635-45c1-b30f-41c6c2164929.us-east4-0.gcp.cloud.qdrant.io"
 
 # creating the config attributes
-collection_name="memory" # this is the collection I created in QDRABT cloud
+collection_name="memory" # this is the collection I created in QDRANT cloud
 api_key=os.environ.get("QDRANT_API_KEY") # Getting the QDRANT api KEY
 host=quadrant_host
 port=6333 #Default port for QDRANT cloud
@@ -21,9 +20,9 @@ config = {
     "vector_store": {
         "provider": "qdrant",
         "config": {
-            "collection_name": "memory",
+            "collection_name": collection_name,
             "host": host,
-            "port": 6333,
+            "port": port,
             "path": None,
             "api_key":api_key
         }
