903f7eb78a4dd23291ac599271c5eb219d85e9b5
PranavPuranik <pranavpuranik10@gmail.com>
2024-08-12 22:00:06 -0500

    final

---
+++ embedchain/embedchain/llm/octoai.py
@@ -0,0 +1,34 @@
+import os
+from typing import Optional
+
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain_community.llms.octoai_endpoint import OctoAIEndpoint
+
+from embedchain.config import BaseLlmConfig
+from embedchain.helpers.json_serializable import register_deserializable
+from embedchain.llm.base import BaseLlm
+
+
+@register_deserializable
+class OctoAILlm(BaseLlm):
+    def __init__(self, config: Optional[BaseLlmConfig] = None):
+        assert "OCTOAI_API_TOKEN" in os.environ, "Please set OCTOAI_API_TOKEN as environment variable."
+        super().__init__(config=config)
+
+    def get_llm_model_answer(self, prompt):
+        return self._get_answer(prompt, self.config)
+
+    @staticmethod
+    def _get_answer(prompt: str, config: BaseLlmConfig) -> str:
+        chat = OctoAIEndpoint(
+            model_name=config.model,
+            max_tokens=config.max_tokens,
+            temperature=config.temperature,
+            top_p=config.top_p,
+            streaming=config.stream,
+            callbacks=config.callbacks
+            if (not config.stream) or (config.stream and config.callbacks)
+            else [StreamingStdOutCallbackHandler()],
+        )
+
+        return chat.invoke(prompt)
---
+++ embedchain/tests/llm/test_octoai.py
@@ -0,0 +1,36 @@
+import os
+import pytest
+from embedchain.config import BaseLlmConfig
+
+from embedchain.llm.octoai import OctoAILlm
+
+@pytest.fixture
+def octoai_env():
+    os.environ["OCTOAI_API_TOKEN"] = "test_api_token"
+
+@pytest.fixture
+def octoai_llm_config():
+    config = BaseLlmConfig(
+        temperature=0.7,
+        model="llama-2-13b-chat-fp16",
+        max_tokens=50,
+        top_p=0.9,
+    )
+    return config
+
+
+def test_get_answer(octoai_llm_config, octoai_env, mocker):
+    mocked_get_answer = mocker.patch("embedchain.llm.octoai.OctoAILlm._get_answer", return_value="Test answer")
+
+    octoai_llm = OctoAILlm(octoai_llm_config)
+    answer = octoai_llm.get_llm_model_answer("Test query")
+
+    assert answer == "Test answer"
+    mocked_get_answer.assert_called_once()
+
+def test_octo_env_variable(octoai_llm_config):
+
+    with pytest.raises(AssertionError):
+        octoai_llm = OctoAILlm(octoai_llm_config)
+
+
