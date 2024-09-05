c02049387d24bef62ae579bd4d3720fdad20d3a3
Deshraj Yadav <deshraj@gatech.edu>
2024-09-05 14:08:11 -0700

    Update mem0/memory/telemetry.py

--- mem0/memory/telemetry.py
+++ mem0/memory/telemetry.py
@@ -79,5 +79,4 @@ def capture_client_event(event_name, instance, additional_data=None):
     }
     if additional_data:
         event_data.update(additional_data)
-        
     telemetry.capture_event(event_name, event_data)
