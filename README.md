7f3b23d2ca673919eaf4a338187db48ab920afb6
galshubeli <gal@falkordb.com>
2024-09-17 13:59:10 +0300

    update-docs

--- README.md
+++ README.md
@@ -157,7 +157,7 @@ history = m.history(memory_id=<memory_id_1>)
 
 ### Graph Memory
 To initialize Graph Memory you'll need to set up your configuration with graph store providers.
-Currently, we support Neo4j as a graph store provider. You can setup [Neo4j](https://neo4j.com/) locally or use the hosted [Neo4j AuraDB](https://neo4j.com/product/auradb/). 
+Currently, we support FalkorDB and Neo4j as a graph store providers. You can setup [FalkorDB](https://www.falkordb.com/) or [Neo4j](https://neo4j.com/) locally or use the hosted [FalkorDB Cloud](https://app.falkordb.cloud/) or [Neo4j AuraDB](https://neo4j.com/product/auradb/). 
 Moreover, you also need to set the version to `v1.1` (*prior versions are not supported*). 
 Here's how you can do it:
 
@@ -166,11 +166,12 @@ from mem0 import Memory
 
 config = {
     "graph_store": {
-        "provider": "neo4j",
+        "provider": "falkordb",
         "config": {
-            "url": "neo4j+s://xxx",
-            "username": "neo4j",
-            "password": "xxx"
+            "host": "---"
+            "username": "---",
+            "password": "---",
+            "port": "---"
         }
     },
     "version": "v1.1"
--- cookbooks/mem0_graph_memory.py
+++ cookbooks/mem0_graph_memory.py
@@ -1,17 +1,23 @@
+# This example shows how to use graph config to use falkordb graph databese
 import os
 from mem0 import Memory
 from dotenv import load_dotenv
-load_dotenv()
 
+# Loading OpenAI API Key
+load_dotenv()
+OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
+USER_ID = "test"
+
+# Creating the config dict from the environment variables
 config = {
-    "llm": {
+    "llm": { # This is the language model configuration, use your carditionals
         "provider": "openai",
         "config": {
             "model": "gpt-4o-mini",
             "temperature": 0
         }
     },
-    "graph_store": {
+    "graph_store": { # See https://app.falkordb.cloud/ for the carditionals
         "provider": "falkordb",
         "config": {
             "host": os.environ['HOST'],
@@ -23,10 +29,12 @@ config = {
     "version": "v1.1"
 }
 
-m = Memory.from_config(config_dict=config)
+# Create the memory class using from config
+memory = Memory.from_config(config_dict=config)
 
-user_id = "alice123"
-m.add("I like painting", user_id=user_id)
-print(m.add("I hate playing badminton", user_id=user_id))
-print(m.get_all(user_id=user_id))
-m.add("My friend name is john and john has a dog named tommy", user_id='gal')
+# Use the Mem0 to add and search memories
+print(memory.add("I like painting", user_id=USER_ID))
+print(memory.add("I hate playing badminton", user_id=USER_ID))
+print(memory.get_all(user_id=USER_ID))
+print(memory.add("My friend name is john and john has a dog named tommy", user_id=USER_ID))
+print(memory.search("What I like to do", user_id=USER_ID))
