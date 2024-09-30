4472ee534648194ad62199489a8fcc5f0f6a5f3a
Prateek Chhikara <prateekchhikara24@gmail.com>
2024-09-30 16:50:51 -0700

    version bump

--- mem0/memory/main.py
+++ mem0/memory/main.py
@@ -215,6 +215,10 @@ class Memory(MemoryBase):
         if self.version == "v1.1" and self.enable_graph:
             if filters["user_id"]:
                 self.graph.user_id = filters["user_id"]
+            elif filters["agent_id"]:
+                self.graph.agent_id = filters["agent_id"]
+            elif filters["run_id"]:
+                self.graph.run_id = filters["run_id"]
             else:
                 self.graph.user_id = "USER"
             data = "\n".join([msg["content"] for msg in messages if "content" in msg and msg["role"] != "system"])
