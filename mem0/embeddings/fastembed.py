396ed4e3a8a5936c7ae3c5087aec1e7ef0d23145
Anush008 <anushshetty90@gmail.com>
2024-12-12 01:24:35 +0530

    chore: poetry.lock
    Signed-off-by: Anush008 <anushshetty90@gmail.com>

--- mem0/embeddings/fastembed.py
+++ mem0/embeddings/fastembed.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+from mem0.configs.embeddings.base import BaseEmbedderConfig
 from mem0.embeddings.base import EmbeddingBase
 
 try:
@@ -14,13 +17,10 @@ class FastEmbedEmbedding(EmbeddingBase):
     Find the list of supported models at https://qdrant.github.io/fastembed/examples/Supported_Models/.
     """
 
-    def __init__(
-        self,
-        model="BAAI/bge-small-en-v1.5",
-    ) -> None:
-        self.model = model
-        self.dims = 384
-        self._model = TextEmbedding(model_name=model)
+    def __init__(self, config: Optional[BaseEmbedderConfig] = None):
+        super().__init__(config)
+
+        self._model = TextEmbedding(model_name=self.config.model, **self.config.model_kwargs)
 
     def embed(self, text):
         return next(self._model.embed(text)).tolist()
