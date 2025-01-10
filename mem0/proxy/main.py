64cb18e8915ede429510b60703a81731a50bfcfd
spike-spiegel-21 <83648453+spike-spiegel-21@users.noreply.github.com>
2025-01-11 01:47:42 +0530

    entities added in proxy (#2135)

--- mem0/proxy/main.py
+++ mem0/proxy/main.py
@@ -181,8 +181,11 @@ class Completions:
     def _format_query_with_memories(self, messages, relevant_memories):
         # Check if self.mem0_client is an instance of Memory or MemoryClient
 
+        entities = []
         if isinstance(self.mem0_client, mem0.memory.main.Memory):
             memories_text = "\n".join(memory["memory"] for memory in relevant_memories["results"])
+            if relevant_memories.get("relations"):
+                entities = [entity for entity in relevant_memories["relations"]]
         elif isinstance(self.mem0_client, mem0.client.main.MemoryClient):
             memories_text = "\n".join(memory["memory"] for memory in relevant_memories)
-        return f"- Relevant Memories/Facts: {memories_text}\n\n- User Question: {messages[-1]['content']}"
+        return f"- Relevant Memories/Facts: {memories_text}\n\n- Entities: {entities}\n\n- User Question: {messages[-1]['content']}"
