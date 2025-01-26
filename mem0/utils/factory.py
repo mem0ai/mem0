3da6609659862960df212485d02e18fb78322587
Anchit <anchit.ksingh@gmail.com>
2025-01-26 22:38:02 +0530

    new branch with google vector search - (matching engine) with ruff and isort fixes

--- mem0/utils/factory.py
+++ mem0/utils/factory.py
@@ -68,6 +68,7 @@ class VectorStoreFactory:
         "azure_ai_search": "mem0.vector_stores.azure_ai_search.AzureAISearch",
         "redis": "mem0.vector_stores.redis.RedisDB",
         "elasticsearch": "mem0.vector_stores.elasticsearch.ElasticsearchDB",
+        "google_matching_engine": "mem0.vector_stores.google_matching_engine.GoogleMatchingEngine",
     }
 
     @classmethod
