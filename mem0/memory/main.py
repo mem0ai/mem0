c42934b7fbdd07ee7fa33879ff54207390fab30c
Dev Khant <devkhant24@gmail.com>
2025-02-23 00:39:26 +0530

    Formatting and Client changes (#2247)

--- mem0/memory/main.py
+++ mem0/memory/main.py
@@ -178,7 +178,7 @@ class Memory(MemoryBase):
                 retrieved_old_memory.append({"id": mem.id, "text": mem.payload["data"]})
         unique_data = {}
         for item in retrieved_old_memory:
-            unique_data[item['id']] = item
+            unique_data[item["id"]] = item
         retrieved_old_memory = list(unique_data.values())
         logging.info(f"Total existing memories: {len(retrieved_old_memory)}")
 
