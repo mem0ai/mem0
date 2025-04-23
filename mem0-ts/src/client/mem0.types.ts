cc5686bd0d7fc925f2d7797967a5bd2595542baf
Saket Aryan <saketaryan2002@gmail.com>
2025-04-23 12:29:27 +0530

    Added Timestamp (#2579)

--- mem0-ts/src/client/mem0.types.ts
+++ mem0-ts/src/client/mem0.types.ts
@@ -21,6 +21,7 @@ export interface MemoryOptions {
   end_date?: string;
   custom_categories?: custom_categories[];
   custom_instructions?: string;
+  timestamp?: number;
 }
 
 export interface ProjectOptions {
