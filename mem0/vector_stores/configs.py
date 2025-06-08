ca54bcba83cf18a2b002eb6cf8e94bff7ed4597d
WayneCao <shilicaohust@163.com>
2025-06-08 08:56:56 +0800

    Feature: baidu vector database integration

--- mem0/vector_stores/configs.py
+++ mem0/vector_stores/configs.py
@@ -17,6 +17,7 @@ class VectorStoreConfig(BaseModel):
         "pinecone": "PineconeConfig",
         "mongodb": "MongoDBConfig",
         "milvus": "MilvusDBConfig",
+        "mochow": "BaiduDBConfig",
         "upstash_vector": "UpstashVectorConfig",
         "azure_ai_search": "AzureAISearchConfig",
         "redis": "RedisDBConfig",
