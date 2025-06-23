b37c8152bb93432ff950b53b317413ee670c1ead
Akshat Jain <125379408+akshat1423@users.noreply.github.com>
2025-06-23 20:45:01 +0530

    Add: Openmemory Augment support (#3015)

--- openmemory/ui/components/shared/source-app.tsx
+++ openmemory/ui/components/shared/source-app.tsx
@@ -51,6 +51,11 @@ export const constants = {
     icon: <Icon source="/images/enconvo.png" />,
     iconImage: "/images/enconvo.png",
   },
+  augment: {
+    name: "Augment",
+    icon: <Icon source="/images/augment.png" />,
+    iconImage: "/images/augment.png",
+  },
   default: {
     name: "Default",
     icon: <BiEdit size={18} className="ml-1" />,
