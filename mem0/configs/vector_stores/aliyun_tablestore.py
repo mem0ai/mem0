858674a9f11514c17b96e4810c3302cafae053ed
shengye.zsy <shengye.zsy@alibaba-inc.com>
2025-07-08 15:03:40 +0800

    change type of some parameters

--- mem0/configs/vector_stores/aliyun_tablestore.py
+++ mem0/configs/vector_stores/aliyun_tablestore.py
@@ -9,11 +9,11 @@ class AliyunTableStoreConfig(BaseModel):
     access_key_secret: str = Field(description="access_key_secret of tablestore")
     vector_dimension: int = Field(1536, description="dimension of vector")
     sts_token: Optional[str] = Field(None, description="sts_token of tablestore")
-    collection_name: Optional[str] = Field("mem0", description="name of the collection")
-    search_index_name: Optional[str] = Field("mem0_search_index", description="index name")
-    text_field: Optional[str] = Field("text", description="name of the text in table field")
-    embedding_field: Optional[str] = Field("embedding", description="name of the embedding field")
-    vector_metric_type: Optional[str] = Field("VM_COSINE", description="metric type for vector")
+    collection_name: str = Field("mem0", description="name of the collection")
+    search_index_name: str = Field("mem0_search_index", description="index name")
+    text_field: str = Field("text", description="name of the text in table field")
+    embedding_field: str = Field("embedding", description="name of the embedding field")
+    vector_metric_type: str = Field("VM_COSINE", description="metric type for vector")
 
     @model_validator(mode="before")
     @classmethod
