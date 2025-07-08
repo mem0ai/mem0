b94e26568d35829cb66b16bcb53c28edd1061122
Saket Aryan <saketaryan2002@gmail.com>
2025-07-08 11:34:49 +0530

    feat: Memory Exports (#3117)

--- mem0-ts/src/client/telemetry.ts
+++ mem0-ts/src/client/telemetry.ts
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import type { TelemetryClient, TelemetryOptions } from "./telemetry.types";
 
-let version = "2.1.33";
+let version = "2.1.35";
 
 // Safely check for process.env in different environments
 let MEM0_TELEMETRY = true;
