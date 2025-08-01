76cc7c490baeeeb1e38462f7391d678721313263
EnzoFanAccount <enzobiondo11@outlook.com>
2025-08-01 17:38:01 -0300

    fix factory config

--- mem0/utils/factory.py
+++ mem0/utils/factory.py
@@ -89,7 +89,7 @@ class VectorStoreFactory:
         "weaviate": "mem0.vector_stores.weaviate.Weaviate",
         "faiss": "mem0.vector_stores.faiss.FAISS",
         "langchain": "mem0.vector_stores.langchain.Langchain",
-        "s3_vectors": "S3VectorsConfig",
+        "s3_vectors": "mem0.vector_stores.s3_vectors.S3Vectors",
     }
 
     @classmethod
