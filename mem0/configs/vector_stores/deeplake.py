6a59480780d457ab35140335d408c2d1f6b7271e
Yu Shi Jie <yshijie1999@gmail.com>
2025-08-26 11:02:52 -0400

    added DeepLakeConfig

---
+++ mem0/configs/vector_stores/deeplake.py
@@ -0,0 +1,33 @@
+from typing import Any, ClassVar, Dict, Optional
+
+from pydantic import BaseModel, Field, model_validator
+
+
+class DeepLakeConfig(BaseModel):
+    try:
+        import deeplake
+    except ImportError:
+        raise ImportError("The 'deeplake' library is required. Please install it using 'pip install chromadb'.")
+
+    url: str = Field("mem://mem0", description="Default url for the collection")
+    creds: Optional[dict] = Field(None, description="Authentication credentials")
+    token: Optional[str] = Field(None, description="Authentication token")
+    embedding_model_dims: Optional[int] = Field(768, description="Dimension of the embedding vector")
+    quantize: Optional[bool] = Field(False, description="Whether to quantize the vectors")
+    client: Optional[deeplake.Dataset] = Field(None, description="DeepLake dataset client instance")
+
+    @model_validator(mode="before")
+    @classmethod
+    def validate_extra_fields(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+        allowed_fields = set(cls.model_fields.keys())
+        input_fields = set(values.keys())
+        extra_fields = input_fields - allowed_fields
+        if extra_fields:
+            raise ValueError(
+                f"Extra fields not allowed: {', '.join(extra_fields)}. Please input only the following fields: {', '.join(allowed_fields)}"
+            )
+        return values
+
+    model_config = {
+        "arbitrary_types_allowed": True,
+    }
