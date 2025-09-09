90828a6cf6d94476941904b703d31ef266547aa3
Swarnaprakash Udayakumar <swarnaprakash+github@gmail.com>
2025-09-09 15:31:53 -0700

    feat(vector-store): Add Valkey vector store support (#3272)

--- mem0/utils/factory.py
+++ mem0/utils/factory.py
@@ -165,6 +165,7 @@ class VectorStoreFactory:
         "pinecone": "mem0.vector_stores.pinecone.PineconeDB",
         "mongodb": "mem0.vector_stores.mongodb.MongoDB",
         "redis": "mem0.vector_stores.redis.RedisDB",
+        "valkey": "mem0.vector_stores.valkey.ValkeyDB",
         "databricks": "mem0.vector_stores.databricks.Databricks",
         "elasticsearch": "mem0.vector_stores.elasticsearch.ElasticsearchDB",
         "vertex_ai_vector_search": "mem0.vector_stores.vertex_ai_vector_search.GoogleMatchingEngine",
