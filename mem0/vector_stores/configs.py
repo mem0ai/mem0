1b9894a7bdf1dd9d77ecee45b691065722ad0a7c
wlleiiwang <1025164922@qq.com>
2025-10-15 20:52:16 +0800

    Feature: tencent vector db integration

--- mem0/vector_stores/configs.py
+++ mem0/vector_stores/configs.py
@@ -33,6 +33,7 @@ class VectorStoreConfig(BaseModel):
         "faiss": "FAISSConfig",
         "langchain": "LangchainConfig",
         "s3_vectors": "S3VectorsConfig",
+        "tencent": "TencentVectorDBConfig",
     }
 
     @model_validator(mode="after")
