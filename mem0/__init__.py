62e459b3002b6d57a5b51f81feb6fea380332b8b
tusharAsync <tushar@actionsync.ai>
2025-12-10 16:11:35 +0530

    Hardcoded version string

--- mem0/__init__.py
+++ mem0/__init__.py
@@ -1,6 +1,6 @@
 import importlib.metadata
 
-__version__ = importlib.metadata.version("mem0ai")
+__version__ = "1.0.1"
 
 from mem0.client.main import AsyncMemoryClient, MemoryClient  # noqa
 from mem0.memory.main import AsyncMemory, Memory  # noqa
