71a7a867f50d719de196c07d27e7b4a39ee25f1d
Kai <moltbotkai@gmail.com>
2026-03-24 12:29:46 +1100

    fix: Resolve ESM import for 'pg' module in PGVector

--- mem0-ts/src/oss/src/vector_stores/pgvector.ts
+++ mem0-ts/src/oss/src/vector_stores/pgvector.ts
@@ -1,4 +1,6 @@
-import { Client } from "pg";
+import pkg from "pg";
+const { Client } = pkg;
+import type { Client as PgClient } from "pg";
 import { VectorStore } from "./base";
 import { SearchFilters, VectorStoreConfig, VectorStoreResult } from "../types";
 
@@ -14,7 +16,7 @@ interface PGVectorConfig extends VectorStoreConfig {
 }
 
 export class PGVector implements VectorStore {
-  private client: Client;
+  private client: PgClient;
   private collectionName: string;
   private useDiskann: boolean;
   private useHnsw: boolean;
