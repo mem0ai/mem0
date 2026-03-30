322f6e8c8765af1f7316b7a32f71c50302b89f85
rakheesingh <rakhisingh2811@gmail.com>
2026-03-30 19:00:31 +0530

    fix the ignore embeddingDims issue for azure

--- mem0-ts/src/oss/src/embeddings/azure.ts
+++ mem0-ts/src/oss/src/embeddings/azure.ts
@@ -5,7 +5,8 @@ import { EmbeddingConfig } from "../types";
 export class AzureOpenAIEmbedder implements Embedder {
   private client: AzureOpenAI;
   private model: string;
-  private embeddingDims?: number;
+  private embeddingDims: number;
+  private passDimensionsToApi: boolean;
 
   constructor(config: EmbeddingConfig) {
     if (!config.apiKey || !config.modelProperties?.endpoint) {
@@ -20,22 +21,31 @@ export class AzureOpenAIEmbedder implements Embedder {
       ...rest,
     });
     this.model = config.model || "text-embedding-3-small";
+    this.passDimensionsToApi = config.embeddingDims != null;
     this.embeddingDims = config.embeddingDims || 1536;
   }
 
   async embed(text: string): Promise<number[]> {
-    const response = await this.client.embeddings.create({
+    const params: Parameters<typeof this.client.embeddings.create>[0] = {
       model: this.model,
       input: text,
-    });
+    };
+    if (this.passDimensionsToApi) {
+      params.dimensions = this.embeddingDims;
+    }
+    const response = await this.client.embeddings.create(params);
     return response.data[0].embedding;
   }
 
   async embedBatch(texts: string[]): Promise<number[][]> {
-    const response = await this.client.embeddings.create({
+    const params: Parameters<typeof this.client.embeddings.create>[0] = {
       model: this.model,
       input: texts,
-    });
+    };
+    if (this.passDimensionsToApi) {
+      params.dimensions = this.embeddingDims;
+    }
+    const response = await this.client.embeddings.create(params);
     return response.data.map((item) => item.embedding);
   }
 }
