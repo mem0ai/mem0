d233da824506ab3516bd86e4f512622f2d4ea633
Mgeeeek <ms8939@bennett.edu.in>
2026-05-14 21:32:29 +0530

    chore: remove redundant trailing newlines in mem0/__init__.py
    Cleans up the placeholder blank lines added in the earlier CI trigger
    commit (#5123). Side-effect: this commit touches mem0/** so it satisfies
    the ci.yml path filter and unblocks the required build_mem0 /
    build_embedchain checks for this docs PR.

--- mem0/__init__.py
+++ mem0/__init__.py
@@ -4,5 +4,3 @@ __version__ = importlib.metadata.version("mem0ai")
 
 from mem0.client.main import AsyncMemoryClient, MemoryClient  # noqa
 from mem0.memory.main import AsyncMemory, Memory  # noqa
-
-
