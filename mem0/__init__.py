b9386af0abcc4c56fa6de6be57a9eb7d2226a62c
Xayar145 <1029229393@qq.com>
2026-08-22 09:11:55 +0800

    fix: fallback gracefully when package metadata is not found

--- mem0/__init__.py
+++ mem0/__init__.py
@@ -1,6 +1,9 @@
 import importlib.metadata
 
-__version__ = importlib.metadata.version("mem0ai")
+try:
+    __version__ = importlib.metadata.version("mem0ai")
+except (importlib.metadata.PackageNotFoundError, Exception):
+    __version__ = "2.0.18"
 
 from mem0.client.main import AsyncMemoryClient, MemoryClient  # noqa
 from mem0.memory.main import AsyncMemory, Memory  # noqa
